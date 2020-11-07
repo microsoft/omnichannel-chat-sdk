1e6fe26b7103df319cc94ff32eccd0ec47465931
Edward Tran <edtran@microsoft.com>
2020-11-07 03:44:16 -0800

    Add tests to create chat adapter

---
+++ __tests__/OmnichannelChatSDK.node.spec.ts
@@ -0,0 +1,48 @@
+const OmnichannelChatSDK = require('../src/OmnichannelChatSDK').default;
+
+/**
+ * @jest-environment node
+ */
+
+describe('Omnichannel Chat SDK (Node)', () => {
+    const omnichannelConfig = {
+        orgUrl: '',
+        orgId: '',
+        widgetId: ''
+    };
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('ChatSDK.createChatAdapter() should not work on React Native platform', async () => {
+        (global as any).navigator = {};
+        (global.navigator as any).product = 'ReactNative';
+        const errorMessage = 'ChatAdapter is only supported on browser';
+
+        const chatSDK = new OmnichannelChatSDK(omnichannelConfig);
+        chatSDK.getChatConfig = jest.fn();
+        chatSDK.getChatToken = jest.fn();
+
+        await chatSDK.initialize();
+
+        chatSDK.OCClient.sessionInit = jest.fn();
+        chatSDK.IC3Client.initialize = jest.fn();
+        chatSDK.IC3Client.joinConversation = jest.fn();
+
+        await chatSDK.startChat();
+
+        try {
+            await chatSDK.createChatAdapter();
+            console.log(chatSDK.createChatAdapter().rejects);
+        } catch (error) {
+            expect(error).toEqual(errorMessage);
+        }
+    });
+
+    afterEach(() => {
+        if (global.navigator) {
+            (global as any).navigator = undefined;
+        }
+    });
+});
---
+++ __tests__/OmnichannelChatSDK.web.spec.ts
@@ -0,0 +1,37 @@
+const OmnichannelChatSDK = require('../src/OmnichannelChatSDK').default;
+
+/**
+ * @jest-environment jsdom
+ */
+
+describe('Omnichannel Chat SDK (Web)', () => {
+    const omnichannelConfig = {
+        orgUrl: '',
+        orgId: '',
+        widgetId: ''
+    };
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('ChatSDK.createChatAdapter() should be returned succesfully on Web platform', async () => {
+        const chatSDK = new OmnichannelChatSDK(omnichannelConfig);
+        chatSDK.getChatConfig = jest.fn();
+        chatSDK.getChatToken = jest.fn();
+
+        await chatSDK.initialize();
+
+        chatSDK.OCClient.sessionInit = jest.fn();
+        chatSDK.IC3Client.initialize = jest.fn();
+        chatSDK.IC3Client.joinConversation = jest.fn();
+
+        await chatSDK.startChat();
+
+        try {
+            await chatSDK.createChatAdapter();
+        } catch (error) {
+            expect(error).not.toBeInstanceOf(Error);
+        }
+    });
+});
