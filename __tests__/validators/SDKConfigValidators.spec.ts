4ce0088d0a3d8484aaf1b0806966fcfc9bf45ec8
Edward Tran <edtran@microsoft.com>
2020-11-10 11:16:37 -0800

    Combine tests

--- __tests__/validators/SDKConfigValidators.spec.ts
+++ __tests__/validators/SDKConfigValidators.spec.ts
@@ -14,6 +14,7 @@ describe('SDKConfigValidators', () => {
     it('Data masking SDK config should to default values if not valid', () => {
         const chatSDKConfig: any = {};
         chatSDKConfig.dataMasking = {
+            disable: 123,
             maskingCharacter: '@@'
         };
 
@@ -21,16 +22,6 @@ describe('SDKConfigValidators', () => {
 
         expect(chatSDKConfig.dataMasking.disable).toBe(defaultChatSDKConfig.dataMasking.disable);
         expect(chatSDKConfig.dataMasking.maskingCharacter).toBe(defaultChatSDKConfig.dataMasking.maskingCharacter);
-    });
-
-    it('Data masking SDK config masking character MUST be a string of length 1', () => {
-        const chatSDKConfig: any = {};
-        chatSDKConfig.dataMasking = {
-            maskingCharacter: ''
-        };
-
-        validateSDKConfig(chatSDKConfig);
-
         expect(chatSDKConfig.dataMasking.maskingCharacter.length).toBe(1);
     });
 });
