a81438026c38beccff2e6147dab488f9151eff90
Edward Tran <edtran@microsoft.com>
2020-12-29 12:50:45 -0800

    Add comments

--- samples/GiftedChatReactNativeControl/src/utils/hooks.ts
+++ samples/GiftedChatReactNativeControl/src/utils/hooks.ts
@@ -1,6 +1,9 @@
 import React, {useEffect} from 'react';
 import { ComponentDidAppearEvent, Navigation, NavigationButtonPressedEvent } from "react-native-navigation";
 
+// Reference: https://github.com/wix/react-native-navigation/issues/4291
+
+// Hook to handle when component appears on the screen
 export function useDidAppearListener(handler: (event: ComponentDidAppearEvent) => void, inputs: any[]) {
     useEffect(
         () => {
@@ -16,6 +19,7 @@ export function useDidAppearListener(handler: (event: ComponentDidAppearEvent) =
     );
 }
 
+// Hook to handle when a TopBar button is pressed
 export function useNavigationButtonPressedListener(handler: (event: NavigationButtonPressedEvent) => void, inputs: any[]) {
     useEffect(
         () => {
