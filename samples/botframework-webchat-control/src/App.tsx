8bd7e2982eb14fbf682b7eb447ae18259a00f9b8
Edward Tran <edtran@microsoft.com>
2021-01-21 03:17:28 -0800

    Install botframework-webchat@4.9.2

--- samples/botframework-webchat-control/src/App.tsx
+++ samples/botframework-webchat-control/src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import ReactWebChat, {createDirectLine} from 'botframework-webchat';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const init = async () => {
+      const res = await fetch('https://webchat-mockbot.azurewebsites.net/directline/token', { method: 'POST' });
+      const { token } = await res.json();
+      setToken(token);
+    }
+
+    init();
+  }, []);
+
+  const chatStyle = {
+    width: '100vw',
+    height: '100vh'
+  };
+
+  const directLine = useMemo(() => createDirectLine({ token }), [token]);
+
   return (
-    <div>
-      <h1> Hello World! </h1>
+    <div style={chatStyle}>
+      <ReactWebChat
+        directLine={directLine}
+      />
     </div>
   );
 }
