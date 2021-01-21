b35bbc38bcdc1f772359acdfcfa52e3489d3e8b6
Edward Tran <edtran@microsoft.com>
2021-01-21 12:02:33 -0800

    Add chat button to start chat

--- samples/botframework-webchat-control/src/App.tsx
+++ samples/botframework-webchat-control/src/App.tsx
@@ -3,6 +3,7 @@ import ReactWebChat from 'botframework-webchat';
 import { OmnichannelChatSDK } from '@microsoft/omnichannel-chat-sdk';
 import './App.css';
 
+
 const omnichannelConfig = {
   orgId: process.env.REACT_APP_orgId || '',
   orgUrl: process.env.REACT_APP_orgUrl || '',
@@ -26,11 +27,6 @@ function App() {
     init();
   }, []);
 
-  const chatStyle = {
-    width: '100vw',
-    height: '100vh'
-  };
-
   const startChat = useCallback(async () => {
     console.log('[startChat]');
     await chatSDK?.startChat();
@@ -48,11 +44,25 @@ function App() {
     setHasChatStarted(false);
   }, [chatSDK]);
 
+  const chatStyle = {
+    width: '60vw',
+    height: '80vh'
+  };
+
+  const iconStyle = {
+    color: 'white'
+  };
+
   return (
     <>
       <div>
-        <button onClick={startChat}> Start </button>
+        {/* <button onClick={startChat}> Start </button> */}
         <button onClick={endChat}> End </button>
+      </div>
+      <div>
+      <div className="chat-button" onClick={startChat}>
+        <i data-feather="message-circle" style={iconStyle}></i>
+      </div>
       </div>
       {
         hasChatStarted && chatAdapter && <div className="chat-container" style={chatStyle}>
