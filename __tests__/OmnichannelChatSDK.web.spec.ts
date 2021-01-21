a90c1322e058479dccedeef23cf884161a36a6b1
Edward Tran <edtran@microsoft.com>
2021-01-20 19:15:21 -0800

    Update test

--- __tests__/OmnichannelChatSDK.web.spec.ts
+++ __tests__/OmnichannelChatSDK.web.spec.ts
@@ -5,7 +5,9 @@
 export {}; // Fix for "Cannot redeclare block-scoped variable 'OmnichannelChatSDK'"
 
 const OmnichannelChatSDK = require('../src/OmnichannelChatSDK').default;
+import libraries from "../src/utils/libraries";
 import platform from "../src/utils/platform";
+import WebUtils from "../src/utils/WebUtils";
 
 describe('Omnichannel Chat SDK (Web)', () => {
     const omnichannelConfig = {
@@ -31,8 +33,13 @@ describe('Omnichannel Chat SDK (Web)', () => {
 
         await chatSDK.startChat();
 
+        spyOn(libraries, 'getIC3AdapterCDNUrl');
+        spyOn(WebUtils, 'loadScript');
+
         try {
             await chatSDK.createChatAdapter();
+            expect(libraries.getIC3AdapterCDNUrl).toHaveBeenCalledTimes(1);
+            expect(WebUtils.loadScript).toHaveBeenCalledTimes(1);
         } catch (error) {
             expect(error).not.toBeInstanceOf(Error);
         }
