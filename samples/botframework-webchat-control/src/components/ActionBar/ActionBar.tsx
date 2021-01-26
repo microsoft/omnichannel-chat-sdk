179061eb46ecbf33bf3cc926cc2199a0d56cb73d
Edward Tran <edtran@microsoft.com>
2021-01-26 13:11:27 -0800

    Add hover effect on buttons

--- samples/botframework-webchat-control/src/components/ActionBar/ActionBar.tsx
+++ samples/botframework-webchat-control/src/components/ActionBar/ActionBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Download, Mail } from "react-feather";
 import './ActionBar.css';
 
@@ -8,22 +8,45 @@ interface ActionBarProps {
 }
 
 function ActionBar (props: ActionBarProps) {
-    return (
-      <div className="action-bar">
-        <Download
-          size={'16'}
-          color='rgb(22, 27, 34)'
-          className='download-button'
-          onClick={props.onDownloadClick}
-        />
-        <Mail
-          size={'16'}
-          color='rgb(22, 27, 34)'
-          className='email-button'
-          onClick={props.onEmailTranscriptClick}
-        />
-      </div>
-    );
+  const [isDownloadFocused, setIsDownloadFocused] = useState(false);
+  const [isEmailTranscriptFocused, setIsEmailTranscriptFocused] = useState(false);
+
+  const onMouseEnterDownload = useCallback(() => {
+    setIsDownloadFocused(true);
+  }, []);
+
+  const onMouseLeaveDownload = useCallback(() => {
+    setIsDownloadFocused(false);
+  }, []);
+
+  const onMouseEnterEmailTranscript = useCallback(() => {
+    setIsEmailTranscriptFocused(true);
+  }, []);
+
+  const onMouseLeaveEmailTranscript = useCallback(() => {
+    setIsEmailTranscriptFocused(false);
+  }, []);
+
+  return (
+    <div className="action-bar">
+      <Download
+        size={'16'}
+        color={isDownloadFocused? 'rgb(22, 27, 34)': 'rgb(22, 27, 34, 0.5)'}
+        className='download-button'
+        onClick={props.onDownloadClick}
+        onMouseEnter={onMouseEnterDownload}
+        onMouseLeave={onMouseLeaveDownload}
+      />
+      <Mail
+        size={'16'}
+        color={isEmailTranscriptFocused? 'rgb(22, 27, 34)': 'rgb(22, 27, 34, 0.5)'}
+        className='email-button'
+        onClick={props.onEmailTranscriptClick}
+        onMouseEnter={onMouseEnterEmailTranscript}
+        onMouseLeave={onMouseLeaveEmailTranscript}
+      />
+    </div>
+  );
 }
 
 export default ActionBar;
