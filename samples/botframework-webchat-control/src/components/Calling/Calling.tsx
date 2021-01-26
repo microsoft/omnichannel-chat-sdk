f26e5529dc97538c62cc0fba8b4b3ff4d85b6f85
Edward Tran <edtran@microsoft.com>
2021-01-25 18:49:06 -0800

    Add incoming call pop up

--- samples/botframework-webchat-control/src/components/Calling/Calling.tsx
+++ samples/botframework-webchat-control/src/components/Calling/Calling.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useState, useContext } from 'react';
+import { Mic, PhoneOff, Video } from 'react-feather';
 import './Calling.css';
 
 interface CallingProps {
@@ -7,8 +8,29 @@ interface CallingProps {
   chatToken: any;
 }
 
+const adjustWebChatHeightInACall = () => {
+  // Adjust height
+  const webChatTranscriptContainer = document.getElementsByClassName('webchat__basic-transcript__scrollable')[0] as HTMLElement;
+  const remoteVideoContainer = document.getElementById('remoteVideo') as HTMLElement;
+
+  const heightGap = webChatTranscriptContainer.clientHeight - remoteVideoContainer.clientHeight;
+  webChatTranscriptContainer.style.marginTop = `${remoteVideoContainer.clientHeight}px`;
+  webChatTranscriptContainer.style.height = `${heightGap}px`;
+}
+
+const adjustWebChatHeightIncomingCall = () => {
+    // Adjust height
+    const webChatTranscriptContainer = document.getElementsByClassName('webchat__basic-transcript__scrollable')[0] as HTMLElement;
+    const incomingCallContainer = document.getElementsByClassName('incoming-call-pop-up')[0] as HTMLElement;
+
+    const heightGap = webChatTranscriptContainer.clientHeight - incomingCallContainer.clientHeight;
+    webChatTranscriptContainer.style.marginTop = `${incomingCallContainer.clientHeight}px`;
+    webChatTranscriptContainer.style.height = `${heightGap}px`;
+}
+
 function Calling(props: CallingProps) {
   const [incomingCall, setIncomingCall] = useState(false);
+  const [inACall, setInACall] = useState(false);
 
   useEffect(() => {
     const init = async () => {
@@ -32,18 +54,8 @@ function Calling(props: CallingProps) {
 
       (VoiceVideoCallingSDK as any).onCallAdded(() => {
         console.log('[WebChat][CallAdded]');
-        (VoiceVideoCallingSDK as any).acceptCall({
-          withVideo: true
-        })
-
-        // Fix height
-        const webChatTranscriptContainer = document.getElementsByClassName('webchat__basic-transcript__scrollable')[0] as HTMLElement;
-        const remoteVideoContainer = document.getElementById('remoteVideo') as HTMLElement;
-
-        const heightGap = webChatTranscriptContainer.clientHeight - remoteVideoContainer.clientHeight;
-        webChatTranscriptContainer.style.marginTop = `${remoteVideoContainer.clientHeight}px`;
-        webChatTranscriptContainer.style.height = `${heightGap}px`;
         setIncomingCall(true);
+        adjustWebChatHeightIncomingCall();
       });
 
       (VoiceVideoCallingSDK as any).onCallDisconnected(() => {
@@ -60,6 +72,7 @@ function Calling(props: CallingProps) {
         webChatTranscriptContainer.style.height = '';
 
         setIncomingCall(false);
+        setInACall(false);
       });
 
       (VoiceVideoCallingSDK as any).onLocalVideoStreamAdded(() => {
@@ -82,13 +95,61 @@ function Calling(props: CallingProps) {
     init();
   }, []);
 
+  const acceptVoiceCall = useCallback(async() => {
+    console.log(`[Calling][Accept][Voice]`);
+    const {VoiceVideoCallingSDK} = props;
+
+    (VoiceVideoCallingSDK as any).acceptCall({
+      withVideo: false
+    });
+
+    setIncomingCall(false);
+    setInACall(true);
+
+    adjustWebChatHeightInACall();
+  }, [props.VoiceVideoCallingSDK, incomingCall]);
+
+  const acceptVideoCall = useCallback(async() => {
+    console.log(`[Calling][Accept][Video]`);
+    const {VoiceVideoCallingSDK} = props;
+
+    await (VoiceVideoCallingSDK as any).acceptCall({
+      withVideo: true
+    });
+
+    setIncomingCall(false);
+    setInACall(true);
+
+    adjustWebChatHeightInACall();
+  }, [props.VoiceVideoCallingSDK, incomingCall]);
+
+  const rejectCall = useCallback(async() => {
+    console.log(`[Calling][Reject]`);
+    const {VoiceVideoCallingSDK} = props;
+
+    await (VoiceVideoCallingSDK as any).rejectCall();
+
+    setIncomingCall(false);
+  }, [props.VoiceVideoCallingSDK, incomingCall]);
+
   return (
-    <div className={`calling ${incomingCall? 'active': ''}`}>
-      <div className="container">
-        <div id="remoteVideo"></div>
-        <div id="selfVideo"></div>
-      </div>
+    <>
+    <div className={`calling ${inACall? 'active': ''}`}>
+      {
+        inACall && <div className="container">
+          <div id="remoteVideo"></div>
+          <div id="selfVideo"></div>
+        </div>
+      }
     </div>
+    {
+      incomingCall && <div className="incoming-call-pop-up">
+        <Video className="accept-video-call-button" onClick={acceptVideoCall} />
+        <Mic className="accept-voice-call-button" onClick={acceptVoiceCall}/>
+        <PhoneOff className="reject-call-button" onClick={rejectCall}/>
+      </div>
+    }
+    </>
   )
 }
 
