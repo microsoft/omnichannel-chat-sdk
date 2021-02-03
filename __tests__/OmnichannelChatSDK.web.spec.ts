4d027bc2f5b2c6bbb09fc8a4b843aa5a0a638eba
xTEddie <edwardtranv1@gmail.com>
2021-02-02 18:13:37 -0800

    Escalation to Voice & Video Web Support (#9)
    * Add web util function to load .js files dynamically
    
    * Add method to fetch CallingSDK from LCW's CDN
    
    * Rename method & Add optional params
    
    * Fix onAgentEndSession triggered on accept voice & video call
    
    * Add error handler on loading spool sdk
    
    * Refactor
    
    * Refactor
    
    * Add proxy for VoiceVideoCalling
    
    * Encapsulate callId & SimplifyVoiceVideoCallingProxy
    
    * Add check to load VoiceVideoCalling if not loaded
    
    * Expose method to handle existing events
    
    * Send calls to OC on accept/reject/end voice/video call
    
    * Prevent VoiceVideoCalling library to load when NoCalling option is set
    
    * Add tests for WebUtils
    
    * Fix typo
    
    * Fix lint
    
    * Rename registerEvent to addEventListener to follow W3C standard
    
    * Add tests
    
    * Add tests
    
    * Add test
    
    * Add test
    
    * Update code coverage
    
    * Ignore console.debug calls for code coverage
    
    * Add test
    
    * Add test
    
    * Add tests
    
    * Add tests
    
    * Update test
    
    * Add test
    
    * Add proxyInstance attribute
    
    * Rename dispose method to close to follow W3C standard
    
    * Fix test
    
    * Fix typo
    
    * Add stop call method
    
    * Return promises & errors
    
    * Fix remote video not rendering properly on consecutive calls by clearing remote video element children in the DOM
    
    * Add test
    
    * Add documentation for Escalation to Voice & Video
    
    * Fix doc
    
    * Add method to get data masking rules
    
    * Add doc for get data masking rules method
    
    * Fix lint
    
    * Update doc for accept call method
    
    * Add comment
    
    * Replace console.log() to console.debug()
    
    * Remove extra line
    
    * Remove extra line
    
    * Add new callId attribute & Refactor
    
    * Check for length

--- __tests__/OmnichannelChatSDK.web.spec.ts
+++ __tests__/OmnichannelChatSDK.web.spec.ts
@@ -5,6 +5,10 @@
 export {}; // Fix for "Cannot redeclare block-scoped variable 'OmnichannelChatSDK'"
 
 const OmnichannelChatSDK = require('../src/OmnichannelChatSDK').default;
+import libraries from "../src/utils/libraries";
+import platform from "../src/utils/platform";
+import WebUtils from "../src/utils/WebUtils";
+import CallingOptionsOptionSetNumber from "../src/core/CallingOptionsOptionSetNumber";
 
 describe('Omnichannel Chat SDK (Web)', () => {
     const omnichannelConfig = {
@@ -15,6 +19,9 @@ describe('Omnichannel Chat SDK (Web)', () => {
 
     beforeEach(() => {
         jest.clearAllMocks();
+        jest.spyOn(platform, 'isNode').mockReturnValue(true);
+        jest.spyOn(platform, 'isReactNative').mockReturnValue(false);
+        jest.spyOn(platform, 'isBrowser').mockReturnValue(false);
     });
 
     it('ChatSDK.createChatAdapter() should be returned succesfully on Web platform', async () => {
@@ -30,10 +37,63 @@ describe('Omnichannel Chat SDK (Web)', () => {
 
         await chatSDK.startChat();
 
+        spyOn(libraries, 'getIC3AdapterCDNUrl');
+        spyOn(WebUtils, 'loadScript');
+
         try {
             await chatSDK.createChatAdapter();
+            expect(libraries.getIC3AdapterCDNUrl).toHaveBeenCalledTimes(1);
+            expect(WebUtils.loadScript).toHaveBeenCalledTimes(1);
         } catch (error) {
             expect(error).not.toBeInstanceOf(Error);
+        }
+    });
+
+    it('ChatSDK.createChatAdapter() should not work if other protocol was set', async () => {
+        const chatSDK = new OmnichannelChatSDK(omnichannelConfig);
+        chatSDK.getChatConfig = jest.fn();
+        chatSDK.getChatToken = jest.fn();
+
+        await chatSDK.initialize();
+
+        chatSDK.OCClient.sessionInit = jest.fn();
+        chatSDK.IC3Client.initialize = jest.fn();
+        chatSDK.IC3Client.joinConversation = jest.fn();
+
+        await chatSDK.startChat();
+
+        jest.spyOn(platform, 'isNode').mockReturnValue(false);
+        jest.spyOn(platform, 'isReactNative').mockReturnValue(false);
+        jest.spyOn(platform, 'isBrowser').mockReturnValue(true);
+
+        const protocol = 'DirectLine';
+        try {
+            await chatSDK.createChatAdapter(protocol);
+        } catch (error) {
+            expect(error).toEqual(`ChatAdapter for protocol ${protocol} currently not supported`);
+        }
+    });
+
+    it('ChatSDK.getVoiceVideoCalling() should not work if callingOption is set to \'NoCalling\'', async () => {
+        const chatSDK = new OmnichannelChatSDK(omnichannelConfig);
+        chatSDK.getChatConfig = jest.fn();
+        chatSDK.getChatToken = jest.fn();
+
+        await chatSDK.initialize();
+
+        chatSDK.callingOption = CallingOptionsOptionSetNumber.NoCalling;
+        chatSDK.OCClient.sessionInit = jest.fn();
+        chatSDK.IC3Client.initialize = jest.fn();
+        chatSDK.IC3Client.joinConversation = jest.fn();
+
+        await chatSDK.startChat();
+
+        jest.spyOn(platform, 'isNode').mockReturnValue(false);
+
+        try {
+            await chatSDK.getVoiceVideoCalling();
+        } catch (error) {
+            expect(error).toEqual('Voice and video call is not enabled');
         }
     });
 });
---
+++ src/api/createVoiceVideoCalling.ts
@@ -0,0 +1,282 @@
+import IChatToken from "../external/IC3Adapter/IChatToken";
+
+interface IVoiceVideoCallingParams {
+    environment?: string;
+    logger?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+    chatToken: IChatToken;
+    OCClient: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+    selfVideoHTMLElementId: string;
+    remoteVideoHTMLElementId: string;
+}
+
+interface IEventPayload {
+    callId: string;
+    spoolCallId: string;
+}
+
+interface IAcceptCallConfig {
+    withVideo?: boolean;
+}
+
+enum SecondaryChannelType {
+    Voice = 192370000,
+    Video = 192380000,
+    Cobrowse = 192390000
+}
+
+enum SecondaryChannelEvents {
+    Accept = 'accept',
+    Reject = 'reject',
+    Connect = 'connect',
+    End = 'end'
+}
+
+export class VoiceVideoCallingProxy {
+    private static _instance: VoiceVideoCallingProxy;
+    private debug: boolean;
+    private logger: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+    private proxy: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+    private proxyInstance: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+    private callingParams?: IVoiceVideoCallingParams;
+    private callId?: string;
+
+    private constructor() {
+        this.debug = false;
+        this.proxy = window.Microsoft.OmniChannel.SDK.VoiceVideoCalling;
+        this.proxyInstance = this.proxy.getInstance();
+    }
+
+    public static getInstance(): VoiceVideoCallingProxy {
+        if (!this._instance) {
+            this._instance = new VoiceVideoCallingProxy();
+        }
+        return this._instance;
+    }
+
+    /* istanbul ignore next */
+    public setDebug(flag: boolean): void {
+        this.debug = flag;
+    }
+
+    public async load(params: any = {}): Promise<void> { // eslint-disable-line @typescript-eslint/no-explicit-any
+        if (params.logger) {
+            this.logger = params.logger;
+        }
+
+        this.proxyInstance.load(params);
+    }
+
+    public isInitialized(): boolean {
+        return this.proxyInstance.isInitialized();
+    }
+
+    public async initialize(params: IVoiceVideoCallingParams): Promise<void> {
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy] VoiceVideoCallingParams: ${JSON.stringify(params)}`);
+        this.callingParams = params;
+        this.callId = this.callingParams?.chatToken.chatId;
+
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy][initialize] _isLoaded: ${this.proxyInstance._isLoaded}`);
+        if (!this.proxyInstance._isLoaded) {
+            await this.proxyInstance.load(this.logger || {});
+        }
+
+        this.proxyInstance.initialize({
+            skypeid: this.callingParams?.chatToken.visitorId,
+            accesstoken: this.callingParams?.chatToken.token,
+            environment: this.callingParams?.environment || 'prod',
+            selfVideoHTMLElementId: this.callingParams?.selfVideoHTMLElementId,
+            remoteVideoHTMLElementId: this.callingParams?.remoteVideoHTMLElementId
+        });
+    }
+
+    public addEventListener(eventName: string, callback: Function): void {
+        this.proxyInstance.registerEvent(eventName, (params: IEventPayload) => {
+            const {callId} = params;
+            /* istanbul ignore next */
+            this.debug && console.debug(`[VoiceVideoCallingProxy][${eventName}] callId: ${callId}`);
+            if (callId !== this.callingParams?.chatToken.chatId) {
+                return;
+            }
+            callback(params);
+        });
+    }
+
+    public isMicrophoneMuted(): boolean {
+        const {callId} = this;
+        return this.proxyInstance.isMicrophoneMuted({callId});
+    }
+
+    public async acceptCall(params: IAcceptCallConfig = {}): Promise<void> {
+        const {callId} = this;
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy][acceptCall] callId: ${callId}`);
+        /* istanbul ignore next */
+        this.debug && console.debug(params);
+
+        this.proxyInstance.acceptCall({callId, withVideo: params.withVideo || false});
+
+        const body = {
+            SecondaryChannelType: params.withVideo? SecondaryChannelType.Video: SecondaryChannelType.Voice,
+            SecondaryChannelEventType: SecondaryChannelEvents.Accept
+        }
+
+        try {
+            this.callingParams?.OCClient.makeSecondaryChannelEventRequest(this.callingParams?.chatToken.requestId, body);
+            /* istanbul ignore next */
+            this.debug && console.debug(`[VoiceVideoCallingProxy][acceptCall][makeSecondaryChannelEventRequest]`);
+        } catch (e) {
+            console.error(`[VoiceVideoCallingProxy][acceptCall][makeSecondaryChannelEventRequest] Failure ${e}`);
+        }
+    }
+
+    public async rejectCall(): Promise<void> {
+        const {callId} = this;
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy][rejectCall] callId: ${callId}`);
+        this.proxyInstance.rejectCall({callId});
+
+        const body = {
+            SecondaryChannelType: this.isRemoteVideoEnabled()? SecondaryChannelType.Video: SecondaryChannelType.Voice,
+            SecondaryChannelEventType: SecondaryChannelEvents.Reject
+        }
+
+        try {
+            this.callingParams?.OCClient.makeSecondaryChannelEventRequest(this.callingParams?.chatToken.requestId, body);
+            /* istanbul ignore next */
+            this.debug && console.debug(`[VoiceVideoCallingProxy][rejectCall][makeSecondaryChannelEventRequest]`);
+        } catch (e) {
+            console.error(`[VoiceVideoCallingProxy][rejectCall][makeSecondaryChannelEventRequest] Failure ${e}`);
+            return e;
+        }
+    }
+
+    public async stopCall(): Promise<void> {
+        const {callId} = this;
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy][stopCall] callId: ${callId}`);
+        this.clearRemoteVideoElementChildren();
+        return this.proxyInstance.stopCall({callId});
+    }
+
+    public async toggleMute(): Promise<void> {
+        const {callId} = this;
+        return this.proxyInstance.toggleMute({callId});
+    }
+
+    public isRemoteVideoEnabled(): boolean {
+        const {callId} = this;
+        return this.proxyInstance.isRemoteVideoEnabled({callId});
+    }
+
+    public isLocalVideoEnabled(): boolean {
+        const {callId} = this;
+        return this.proxyInstance.isLocalVideoEnabled({callId});
+    }
+
+    public async toggleLocalVideo(): Promise<void> {
+        const {callId} = this;
+        return this.proxyInstance.toggleLocalVideo({callId});
+    }
+
+    public isInACall(): boolean {
+        const {callId} = this;
+        return this.proxyInstance.isInACall({callId});
+    }
+
+    public renderVideoStreams(): void {
+        const {callId} = this;
+        return this.proxyInstance.renderVideoStreams({callId});
+    }
+
+    public disposeVideoRenderers(): void {
+        const {callId} = this;
+        return this.proxyInstance.disposeVideoRenderers({callId});
+    }
+
+    public close(): void {
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy][close]`);
+        this.proxyInstance.dispose();
+        this.callingParams = undefined;
+        this.callId = undefined;
+    }
+
+    public onCallAdded(callback: Function): void {
+        const eventName = 'callAdded';
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy][${eventName}]`);
+        this.addEventListener(eventName, callback);
+    }
+
+    public onLocalVideoStreamAdded(callback: Function): void {
+        const eventName = 'localVideoStreamAdded';
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy][${eventName}]`);
+        this.addEventListener(eventName, callback);
+    }
+
+    public onLocalVideoStreamRemoved(callback: Function): void {
+        const eventName = 'localVideoStreamRemoved';
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy][${eventName}]`);
+        this.addEventListener(eventName, callback);
+    }
+
+    public onRemoteVideoStreamAdded(callback: Function): void {
+        const eventName = 'remoteVideoStreamAdded';
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy][${eventName}]`);
+        this.addEventListener(eventName, callback);
+    }
+
+    public onRemoteVideoStreamRemoved(callback: Function): void {
+        const eventName = 'remoteVideoStreamRemoved';
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy][${eventName}]`);
+        this.addEventListener(eventName, callback);
+    }
+
+    public onCallDisconnected(callback: Function): void {
+        const eventName = 'callDisconnected';
+        /* istanbul ignore next */
+        this.debug && console.debug(`[VoiceVideoCallingProxy][${eventName}]`);
+        this.addEventListener(eventName, async (params: IEventPayload) => {
+            const body = {
+                SecondaryChannelType: SecondaryChannelType.Voice,
+                SecondaryChannelEventType: SecondaryChannelEvents.End
+            }
+
+            try {
+                this.callingParams?.OCClient.makeSecondaryChannelEventRequest(this.callingParams?.chatToken.requestId, body);
+                this.debug && console.debug(`[VoiceVideoCallingProxy][onCallDisconnected][makeSecondaryChannelEventRequest]`);
+            } catch (e) {
+                console.error(`[VoiceVideoCallingProxy][onCallDisconnected][makeSecondaryChannelEventRequest] Failure ${e}`);
+            }
+
+            this.clearRemoteVideoElementChildren();
+            callback(params);
+        });
+    }
+
+    private clearRemoteVideoElementChildren() {
+        // Remove remoteVideoHTMLElement children if any or video won't be rendered properly
+        if (this.callingParams?.remoteVideoHTMLElementId) {
+            const remoteVideoElement = document.getElementById(this.callingParams?.remoteVideoHTMLElementId);
+            while (remoteVideoElement?.firstChild) {
+                /* istanbul ignore next */
+                this.debug && console.debug('[VoiceVideoCallingProxy][clearRemoteVideoElement]');
+                remoteVideoElement.firstChild.remove();
+            }
+        }
+    }
+}
+
+const createVoiceVideoCalling = async (params: any = {}): Promise<VoiceVideoCallingProxy> => { // eslint-disable-line @typescript-eslint/no-explicit-any
+    const proxy = VoiceVideoCallingProxy.getInstance();
+    await proxy.load(params.logger);
+    return Promise.resolve(proxy);
+}
+
+export default createVoiceVideoCalling;
