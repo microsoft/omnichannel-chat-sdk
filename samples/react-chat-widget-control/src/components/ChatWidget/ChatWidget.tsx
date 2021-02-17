94f3b92c2d41c0e220f7210b93040a4126503ede
Edward Tran <edtran@microsoft.com>
2021-02-17 09:43:02 -0800

    Install react-chat-widget

--- samples/react-chat-widget-control/src/components/ChatWidget/ChatWidget.tsx
+++ samples/react-chat-widget-control/src/components/ChatWidget/ChatWidget.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Widget, addResponseMessage } from 'react-chat-widget';
+import 'react-chat-widget/lib/styles.css';
 
 function ChatWidget() {
+  useEffect(() => {
+    // Sample message from agent
+    addResponseMessage('Hi, how can I help you?');
+  }, []);
+
+  const handleNewUserMessage = (newMessage: any) => {
+    console.log(`New message incoming! ${newMessage}`);
+
+    // TODO: Send message to Omnichannel
+  };
+
   return (
     <div>
       <h1> Hello World!</h1>
+      <Widget
+        handleNewUserMessage={handleNewUserMessage}
+      />
     </div>
   );
 }
