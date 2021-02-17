ac9493ef2c789dd556ebcf930b88efbf020f06ba
Edward Tran <edtran@microsoft.com>
2021-02-17 10:19:13 -0800

    Add logic to check for chat widget open state

--- samples/react-chat-widget-control/src/components/ChatWidget/ChatWidget.tsx
+++ samples/react-chat-widget-control/src/components/ChatWidget/ChatWidget.tsx
@@ -1,9 +1,32 @@
-import React, { useEffect } from 'react';
-import { Widget, addResponseMessage } from 'react-chat-widget';
+import React, { useEffect, useState } from 'react';
+import { Widget, addResponseMessage, isWidgetOpened, toggleWidget } from 'react-chat-widget';
+import fetchOmnichannelConfig from '../../utils/fetchOmnichannelConfig';
+import {OmnichannelChatSDK} from '@microsoft/omnichannel-chat-sdk';
 import 'react-chat-widget/lib/styles.css';
 
+const omnichannelConfig: any = fetchOmnichannelConfig();
+
+console.log(`%c [OmnichannelConfig]`, 'background-color:#001433;color:#fff');
+console.log(omnichannelConfig);
+
 function ChatWidget() {
+  const [chatSDK, setChatSDK] = useState<OmnichannelChatSDK>();
+  const [open, setOpen] = useState<boolean>(false);
+
   useEffect(() => {
+    const init = async () => {
+      const chatSDK = new OmnichannelChatSDK(omnichannelConfig);
+      await chatSDK.initialize();
+      setChatSDK(chatSDK);
+
+      const liveChatContext = localStorage.getItem('liveChatContext');
+      if (liveChatContext && Object.keys(JSON.parse(liveChatContext)).length > 0) {
+        console.log("[liveChatContext]");
+        console.log(liveChatContext);
+      }
+    }
+
+    init();
     // Sample message from agent
     addResponseMessage('Hi, how can I help you?');
   }, []);
@@ -14,12 +37,20 @@ function ChatWidget() {
     // TODO: Send message to Omnichannel
   };
 
+  const onWidgetClick = (event: any) => {
+    const open = isWidgetOpened();
+    console.log(`[isWidgetOpened] ${open}`);
+    setOpen(open);
+  }
+
   return (
     <div>
       <h1> Hello World!</h1>
-      <Widget
-        handleNewUserMessage={handleNewUserMessage}
-      />
+      <div onClick={onWidgetClick}>
+        <Widget
+          handleNewUserMessage={handleNewUserMessage}
+        />
+      </div>
     </div>
   );
 }
