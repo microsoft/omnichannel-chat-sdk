f9df8f653e9027997c162b6f7543f9062a5a2721
Edward Tran <edtran@microsoft.com>
2021-03-19 10:15:58 -0700

    Add ability to provide custom version on getIC3ClientCDNUrl & getIC3AdapterCDNUrl

--- src/utils/libraries.ts
+++ src/utils/libraries.ts
@@ -1,12 +1,12 @@
 import { ic3ClientVersion, webChatIC3AdapterVersion } from "../config/settings";
 
-const getIC3ClientCDNUrl = (): string => {
-    const IC3ClientCDNUrl = `https://comms.omnichannelengagementhub.com/release/${ic3ClientVersion}/Scripts/SDK/SDK.min.js`;
+const getIC3ClientCDNUrl = (version = ic3ClientVersion): string => {
+    const IC3ClientCDNUrl = `https://comms.omnichannelengagementhub.com/release/${version}/Scripts/SDK/SDK.min.js`;
     return IC3ClientCDNUrl;
 }
 
-const getIC3AdapterCDNUrl = (): string => {
-    const IC3AdapterCDNUrl = `https://webchatic3.blob.core.windows.net/webchat-ic3adapter/${webChatIC3AdapterVersion}/botframework-webchat-adapter-ic3.production.min.js`;
+const getIC3AdapterCDNUrl = (version = webChatIC3AdapterVersion): string => {
+    const IC3AdapterCDNUrl = `https://webchatic3.blob.core.windows.net/webchat-ic3adapter/${version}/botframework-webchat-adapter-ic3.production.min.js`;
     return IC3AdapterCDNUrl;
 }
 
