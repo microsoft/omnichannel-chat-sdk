82e8105695cc5dcf637cd5b07a535b0057fef29f
Edward Tran <edtran@microsoft.com>
2021-03-29 12:56:35 -0700

    Add ariaTelemetryKey in config

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,7 +1,9 @@
 const ic3ClientVersion = '2020.08.24.1';
 const webChatIC3AdapterVersion = '0.1.0-master.86ac1a0';
+const ariaTelemetryKey = '';
 
 export {
     ic3ClientVersion,
-    webChatIC3AdapterVersion
+    webChatIC3AdapterVersion,
+    ariaTelemetryKey
 };
