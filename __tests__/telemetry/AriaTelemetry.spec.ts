dd9dca32fa2796553e0e715d7f594bc6ea50e77f
Edward Tran <edtran@microsoft.com>
2021-04-05 08:28:53 -0700

    Add tests

--- __tests__/telemetry/AriaTelemetry.spec.ts
+++ __tests__/telemetry/AriaTelemetry.spec.ts
@@ -15,4 +15,32 @@ describe('AriaTelemetry', () => {
         AriaTelemetry.disable();
         expect((AriaTelemetry as any)._disable).toBe(true);
     });
+
+    it('AriaTelemetry.setCDNPackages() should update AriaTelemetry._CDNPackagesInfo', () => {
+        const packages = {
+            IC3Client: 'IC3Client',
+            IC3Adapter: 'IC3Adapter',
+        };
+
+        AriaTelemetry.setCDNPackages(packages);
+
+        expect((AriaTelemetry as any)._CDNPackagesInfo).toEqual(packages);
+    });
+
+    it('AriaTelemetry.setCDNPackages() should be able to override previous AriaTelemetry._CDNPackagesInfo', () => {
+        const packages = {
+            IC3Client: 'IC3Client',
+            IC3Adapter: 'IC3Adapter',
+        };
+
+        const overridePackages = {
+            IC3Client: 'override',
+            IC3Adapter: 'override',
+        };
+
+        AriaTelemetry.setCDNPackages(packages);
+        AriaTelemetry.setCDNPackages(overridePackages);
+
+        expect((AriaTelemetry as any)._CDNPackagesInfo).toEqual(overridePackages);
+    });
 });
