07e26dd9eeab8ca838b186a7d21e747694d55b32
Edward Tran <edtran@microsoft.com>
2021-04-07 20:27:43 -0700

    Update ariaTelemetryKey

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,6 +1,6 @@
 const ic3ClientVersion = '2020.08.24.1';
 const webChatIC3AdapterVersion = '0.1.0-master.86ac1a0';
-const ariaTelemetryKey = '';
+const ariaTelemetryKey = '07172c8dd08d4de7a594b5b5cf81e668-92304109-4cd9-41b6-8806-714387b256e8-7337';
 
 export {
     ic3ClientVersion,
