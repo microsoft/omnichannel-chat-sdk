3f7e4b69da8f2685ecb99e4b1a636df9d38750f3
xTEddie <edwardtranv1@gmail.com>
2021-04-11 22:08:06 -0700

    Prevent duplicate libraries to be dynamically loaded (#26)
    * Add check to prevent duplicated library to be loaded
    
    * Add initialize flag
    
    * Add test
    
    * Fix eslint

--- __tests__/utils/WebUtils.web.spec.ts
+++ __tests__/utils/WebUtils.web.spec.ts
@@ -5,12 +5,25 @@
 const WebUtils = require('../../src/utils/WebUtils');
 
 describe('WebUtils', () => {
-    it('WebUtils.loadScript() should create <script> element', async () => {
+    it('WebUtils.loadScript() should create <script> element', () => {
         const sampleUrl = 'sample';
 
+        jest.spyOn(document, 'getElementsByTagName').mockReturnValue([] as any);
         jest.spyOn(document, 'createElement');
 
         WebUtils.loadScript(sampleUrl);
         expect(document.createElement).toHaveBeenCalledTimes(1);
+    });
+
+    it('WebUtils.loadSCript() should not create duplicate <script> element', () => {
+        jest.clearAllMocks();
+
+        const sampleUrl = 'sample';
+
+        jest.spyOn(document, 'getElementsByTagName').mockReturnValue([{src: sampleUrl}] as any);
+        jest.spyOn(document, 'createElement');
+
+        WebUtils.loadScript(sampleUrl);
+        expect(document.createElement).toHaveBeenCalledTimes(0);
     });
 });
--- src/utils/WebUtils.ts
+++ src/utils/WebUtils.ts
@@ -1,5 +1,13 @@
 const loadScript = async (scriptUrl: string, callbackOnload: CallableFunction = () => void(0), callbackError: CallableFunction = () => void(0)): Promise<void> => {
-  return new Promise ((resolve, reject) => {
+  return new Promise (async (resolve, reject) => { // eslint-disable-line no-async-promise-executor
+    const scriptElements = Array.from(document.getElementsByTagName('script'));
+    const foundScriptElement = scriptElements.filter(scriptElement => scriptElement.src == scriptUrl);
+
+    if (foundScriptElement.length) {
+      await callbackOnload();
+      return resolve();
+    }
+
     const scriptElement = document.createElement('script');
     scriptElement.setAttribute('src', scriptUrl);
     scriptElement.setAttribute('type', 'text/javascript');
