b9d4cfdc8495e959fb4af5c7d655e786b1d1b767
Edward Tran <edtran@microsoft.com>
2021-04-19 18:07:44 -0700

    Add sdk in IIC3AdapterOptions

--- src/external/IC3Adapter/IIC3AdapterOptions.ts
+++ src/external/IC3Adapter/IIC3AdapterOptions.ts
@@ -7,6 +7,7 @@ export default interface IIC3AdapterOptions {
     conversation?: IConversation
     chatToken?: IChatToken;
     sdkURL?: string;
+    sdk?: any;
     // visitor: boolean;
     // sendHeartBeat: boolean;
     // hostType: HostType;
