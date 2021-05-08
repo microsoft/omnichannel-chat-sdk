0c3497a48dcd2e8353a6a6717e5ae2bdb5043eea
Edward Tran <edtran@microsoft.com>
2021-05-08 11:12:07 -0700

    Add checks for LiveChatV2 messages

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -1,12 +1,22 @@
 import MessageType from "@microsoft/omnichannel-ic3core/lib/model/MessageType";
+import ACSParticipantDisplayName from "../core/ACSParticipantDisplayName";
 
 export const isSystemMessage = (message: any): boolean => { // eslint-disable-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
-    const {messageType, properties} = message;
-    return (messageType === MessageType.UserMessage)
-    && (properties.tags.includes("system"));
+    const {messageType, properties, sender} = message;
+
+    const conditionV1 = messageType === MessageType.UserMessage
+    && properties.tags
+    && properties.tags.includes("system");
+
+    const conditionV2 = sender.alias && sender.alias === ACSParticipantDisplayName.System;
+    return conditionV1 || conditionV2;
 }
 
 export const isCustomerMessage = (message: any): boolean => { // eslint-disable-line @typescript-eslint/no-explicit-any,  @typescript-eslint/explicit-module-boundary-types
     const {sender} = message;
-    return (sender.id.includes('contacts/8:'));
+
+    const conditionV1 = sender.id && sender.id.includes('contacts/8:');
+    const conditionV2 = sender.alias && sender.alias === ACSParticipantDisplayName.Customer;
+
+    return conditionV1 || conditionV2;
 }
