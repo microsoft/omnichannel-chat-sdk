abcaf3e862dcd7ec1a237ce0a4642ed4c403f5e8
Edward Tran <edtran@microsoft.com>
2021-09-10 23:36:40 -0700

    Implement AMSFileManager

--- src/external/ACSAdapter/AMSFileManager.ts
+++ src/external/ACSAdapter/AMSFileManager.ts
@@ -1,39 +1,92 @@
 import {FileMetadata, IFileManager, IFileUploadRequest, IUploadedFile, PermissionsOptions} from "acs_webchat-chat-adapter/src/types/FileManagerTypes";
 import FramedClient from "@microsoft/omnichannel-amsclient/lib/FramedClient";
 
-class AMSFileManager implements IFileManager {
+class AMSFileManager {
     private amsClient: FramedClient;
 
     public constructor(amsClient: FramedClient) {
         this.amsClient = amsClient;
     }
 
-    uploadFiles(files: IFileUploadRequest[]): Promise<IUploadedFile[]> {
-        throw new Error("Method not implemented.");
+    public async uploadFiles(files: IFileUploadRequest[]): Promise<IUploadedFile[]> {
+        return Promise.all(files.map(async (file: IFileUploadRequest) => this.uploadFileToAMS(file))) as Promise<IUploadedFile[]>;
     }
 
-    downloadFiles(files: IUploadedFile[]): Promise<File[]> {
-        throw new Error("Method not implemented.");
+    public async downloadFiles(files: IUploadedFile[]): Promise<File[]> {
+        return Promise.all(files.map(async (file: IUploadedFile) => this.downloadFileFromAMS(file))) as Promise<File[]>;
     }
 
-    updatePermissions(file: IUploadedFile, permissions: PermissionsOptions): Promise<void> {
-        throw new Error("Method not implemented.");
+    public async updatePermissions(): Promise<void> {
+        throw new Error('Method not implemented.');
     }
 
-    getFileIds(metadata?: Record<string, string>): string[] | undefined {
-        throw new Error("Method not implemented.");
+    public getFileIds(metadata?: Record<string, string>): string[] | undefined {
+        try {
+            return JSON.parse(metadata?.amsReferences as string) as string[];
+        } catch {
+
+        }
     }
 
-    createFileIdProperty(fileIds: string[]): Record<string, string> {
-        throw new Error("Method not implemented.");
+    public createFileIdProperty(fileIds: string[]): Record<string, string> | undefined {
+        try {
+            return {
+                amsReferences: JSON.stringify(fileIds)
+            } as Record<string, string>;
+        } catch {
+
+        }
     }
 
-    getFileMetadata(metadata?: Record<string, string>): FileMetadata[] | undefined {
-        throw new Error("Method not implemented.");
+    public getFileMetadata(metadata?: Record<string, string>): FileMetadata[] | undefined {
+        try {
+            return JSON.parse(metadata?.amsMetadata as string);
+        } catch {
+
+        }
     }
 
-    createFileMetadataProperty(metadata: FileMetadata[]): Record<string, string> {
-        throw new Error("Method not implemented.");
+    public createFileMetadataProperty(metadata: FileMetadata[]): Record<string, string> | undefined {
+        try {
+            return {
+                amsMetadata: JSON.stringify(metadata)
+            };
+        } catch {
+
+        }
+    }
+
+    private async uploadFileToAMS(fileToUpload: IFileUploadRequest): Promise<IUploadedFile | undefined> {
+        if (fileToUpload.contentUrl && fileToUpload.name) {
+            const blob = await this.amsClient.fetchBlob(fileToUpload.contentUrl);
+            const file = new File([blob], fileToUpload.name, { type: fileToUpload.contentType });
+            const response: any = await this.amsClient.createObject((this.amsClient as any).chatToken.chatId, file);
+
+            await this.amsClient.uploadDocument(response.id, file);
+
+            return {
+                fileId: response.id,
+                metadata: {
+                    contentType: fileToUpload.contentType,
+                    fileName: fileToUpload.name
+                }
+            }
+        }
+    }
+
+    private async downloadFileFromAMS(uploadedFile: IUploadedFile): Promise<File | undefined> {
+        if (uploadedFile.fileId && uploadedFile.metadata && uploadedFile.metadata.fileName) {
+            const fileMetadata = {
+                id: uploadedFile.fileId,
+                type: uploadedFile.metadata.contentType.split("/").pop() as string
+            };
+
+            const response: any = await this.amsClient.getViewStatus(fileMetadata);
+            const {view_location} = response;
+            const blob: any = await this.amsClient.getView(fileMetadata, view_location);
+            const file = new File([blob], uploadedFile.metadata.fileName, { type: uploadedFile.metadata.contentType });
+            return file;
+        }
     }
 }
 
