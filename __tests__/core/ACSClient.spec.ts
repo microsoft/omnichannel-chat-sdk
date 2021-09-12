054284b23c4c27832bc5800f9d36853c7182f2b8
Edward Tran <edtran@microsoft.com>
2021-09-12 04:41:44 -0700

    Add test

--- __tests__/core/ACSClient.spec.ts
+++ __tests__/core/ACSClient.spec.ts
@@ -47,4 +47,43 @@ describe('ACSClient', () => {
         expect(conversation).toBeDefined();
         expect(conversation.sessionInfo).toBeDefined();
     });
+
+    it('ACSClient.conversation.getMessages() should call ChatThreadClient.listMessages()', async () => {
+        const client: any = new ACSClient();
+        const config = {
+            token: 'token',
+            environmentUrl: 'url'
+        }
+
+        await client.initialize(config);
+
+        const chatThreadClient: any = {};
+        chatThreadClient.listParticipants = jest.fn(() => ({
+            next: jest.fn(() => ({
+                value: 'value',
+                done: jest.fn()
+            })),
+        }));
+        chatThreadClient.listMessages = jest.fn(() => ({
+            next: jest.fn(() => ({
+                value: 'value',
+                done: jest.fn()
+            })),
+        }));
+
+        client.chatClient = {};
+        client.chatClient.getChatThreadClient = jest.fn(() => chatThreadClient);
+        client.chatClient.startRealtimeNotifications = jest.fn();
+
+        const conversation = await client.joinConversation({
+            id: 'id',
+            threadId: 'threadId',
+            pollingInterval: 1000,
+        });
+
+        const messages = await conversation.getMessages();
+
+        expect(chatThreadClient.listMessages).toHaveBeenCalledTimes(1);
+        expect(messages).toBeDefined();
+    });
 });
