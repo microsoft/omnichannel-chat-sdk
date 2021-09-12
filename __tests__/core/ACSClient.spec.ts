fdc5aad1423fa2c9146d6a7c713fa1a5cb37281d
Edward Tran <edtran@microsoft.com>
2021-09-12 04:30:32 -0700

    Add test

---
+++ __tests__/core/ACSClient.spec.ts
@@ -0,0 +1,50 @@
+import ACSClient, { ACSConversation } from "../../src/core/ACSClient";
+
+jest.mock('@azure/communication-common');
+jest.mock('@azure/communication-chat');
+
+describe('ACSClient', () => {
+    it('ACSClient.initialize() should create ChatClient', async () => {
+        const client: any = new ACSClient();
+        const config = {
+            token: 'token',
+            environmentUrl: 'url'
+        }
+
+        await client.initialize(config);
+
+        expect(client.tokenCredential).toBeDefined();
+        expect(client.chatClient).toBeDefined();
+    });
+
+    it('ACSClient.joinConversation() should return ACSConversation', async () => {
+        const client: any = new ACSClient();
+        const config = {
+            token: 'token',
+            environmentUrl: 'url'
+        }
+
+        await client.initialize(config);
+
+        const chatThreadClient: any = {};
+        chatThreadClient.listParticipants = jest.fn(() => ({
+            next: jest.fn(() => ({
+                value: 'value',
+                done: jest.fn()
+            })),
+        }));
+
+        client.chatClient = {};
+        client.chatClient.getChatThreadClient = jest.fn(() => chatThreadClient);
+        client.chatClient.startRealtimeNotifications = jest.fn();
+
+        const conversation = await client.joinConversation({
+            id: 'id',
+            threadId: 'threadId',
+            pollingInterval: 1000,
+        });
+
+        expect(conversation).toBeDefined();
+        expect(conversation.sessionInfo).toBeDefined();
+    });
+});
