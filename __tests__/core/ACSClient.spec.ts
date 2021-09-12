00d5e5b92c6764dee3ebc02124cbe436b69bdf93
Edward Tran <edtran@microsoft.com>
2021-09-12 04:58:41 -0700

    Add tests

--- __tests__/core/ACSClient.spec.ts
+++ __tests__/core/ACSClient.spec.ts
@@ -131,6 +131,92 @@ describe('ACSClient', () => {
         expect(client.chatClient.on.mock.calls[0][0]).toEqual(event);
     });
 
+    it('ACSClient.conversation.registerOnThreadUpdate() should register to "participantsRemoved" event', async () => {
+        const client: any = new ACSClient();
+        const config = {
+            token: 'token',
+            environmentUrl: 'url'
+        }
+
+        await client.initialize(config);
+
+        const chatThreadClient: any = {};
+        chatThreadClient.listParticipants = jest.fn(() => ({
+            next: jest.fn(() => ({
+                value: 'value',
+                done: jest.fn()
+            })),
+        }));
+        chatThreadClient.listMessages = jest.fn(() => ({
+            next: jest.fn(() => ({
+                value: 'value',
+                done: jest.fn()
+            })),
+        }));
+
+        client.chatClient = {};
+        client.chatClient.getChatThreadClient = jest.fn(() => chatThreadClient);
+        client.chatClient.startRealtimeNotifications = jest.fn();
+        client.chatClient.on = jest.fn();
+
+        const conversation = await client.joinConversation({
+            id: 'id',
+            threadId: 'threadId',
+            pollingInterval: 1000,
+        });
+
+        jest.spyOn(conversation, 'getMessages');
+
+        await conversation.registerOnThreadUpdate(() => {});
+
+        const event = "participantsRemoved";
+        expect(client.chatClient.on).toHaveBeenCalledTimes(1);
+        expect(client.chatClient.on.mock.calls[0][0]).toEqual(event);
+    });
+
+    it('ACSClient.conversation.onTypingEvent() should register to "typingIndicatorReceived" event', async () => {
+        const client: any = new ACSClient();
+        const config = {
+            token: 'token',
+            environmentUrl: 'url'
+        }
+
+        await client.initialize(config);
+
+        const chatThreadClient: any = {};
+        chatThreadClient.listParticipants = jest.fn(() => ({
+            next: jest.fn(() => ({
+                value: 'value',
+                done: jest.fn()
+            })),
+        }));
+        chatThreadClient.listMessages = jest.fn(() => ({
+            next: jest.fn(() => ({
+                value: 'value',
+                done: jest.fn()
+            })),
+        }));
+
+        client.chatClient = {};
+        client.chatClient.getChatThreadClient = jest.fn(() => chatThreadClient);
+        client.chatClient.startRealtimeNotifications = jest.fn();
+        client.chatClient.on = jest.fn();
+
+        const conversation = await client.joinConversation({
+            id: 'id',
+            threadId: 'threadId',
+            pollingInterval: 1000,
+        });
+
+        jest.spyOn(conversation, 'getMessages');
+
+        await conversation.onTypingEvent(() => {});
+
+        const event = "typingIndicatorReceived";
+        expect(client.chatClient.on).toHaveBeenCalledTimes(1);
+        expect(client.chatClient.on.mock.calls[0][0]).toEqual(event);
+    });
+
     it('ACSClient.conversation.sendFileMessage() should be mocked', async () => {
         const client: any = new ACSClient();
         const config = {
