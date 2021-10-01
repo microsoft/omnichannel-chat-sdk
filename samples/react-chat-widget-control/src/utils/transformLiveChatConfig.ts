902c0b10202e7caccd3c903f7f7656ab4543fc74
Edward Tran <edtran@microsoft.com>
2021-09-30 23:16:46 -0700

    Update typing

--- samples/react-chat-widget-control/src/utils/transformLiveChatConfig.ts
+++ samples/react-chat-widget-control/src/utils/transformLiveChatConfig.ts
@@ -1,10 +1,8 @@
-import IChatConfig from "@microsoft/omnichannel-chat-sdk/lib/core/IChatConfig";
-
 export class ConfigurationManager {
     public static canUploadAttachment: boolean = false;
 }
 
-const transformLiveChatConfig = (liveChatConfig: IChatConfig): ConfigurationManager => {
+const transformLiveChatConfig = (liveChatConfig: any): ConfigurationManager => {
     const canUploadAttachment = (liveChatConfig as any)["LiveWSAndLiveChatEngJoin"]["msdyn_enablefileattachmentsforcustomers"] === "true" || false;
     ConfigurationManager.canUploadAttachment = canUploadAttachment;
     return ConfigurationManager;
