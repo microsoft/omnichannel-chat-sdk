1938e4085a9fcee37ecd4fe437f834f41e29c0f7
Edward Tran <edtran@microsoft.com>
2022-03-10 16:14:04 -0800

    Fix lint

--- src/core/messaging/ChatAdapterOptionalParams.ts
+++ src/core/messaging/ChatAdapterOptionalParams.ts
@@ -2,12 +2,12 @@ interface ChatAdapterOptionalParams {
     protocol?: string;
     IC3Adapter?: {
         options?: {
-            [key: string]: any
+            [key: string]: any // eslint-disable-line @typescript-eslint/no-explicit-any
         }
     }
     ACSAdapter?: {
         options?: {
-            [key: string]: any
+            [key: string]: any // eslint-disable-line @typescript-eslint/no-explicit-any
         }
     }
 }
