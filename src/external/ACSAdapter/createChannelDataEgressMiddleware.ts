2c23bd19b4a127739e749693dd2d2a0448c9066f
Edward Tran <edtran@microsoft.com>
2022-03-22 14:09:21 -0700

    Extract tags to constants

--- src/external/ACSAdapter/createChannelDataEgressMiddleware.ts
+++ src/external/ACSAdapter/createChannelDataEgressMiddleware.ts
@@ -1,3 +1,4 @@
+import { channelIdTag, customerMessageTag } from "../../core/messaging/MessageTags";
 import { DeliveryMode } from "../../core/messaging/OmnichannelMessage";
 
 interface ChannelData {
@@ -5,9 +6,6 @@ interface ChannelData {
 }
 
 const createChannelDataEgressMiddleware = (channelData: ChannelData): CallableFunction => {
-    const channelIdTag = `ChannelId-lcw`; // Tag for PVA bot to return proper response for live chat channel
-    const customerMessageTag = `FromCustomer`; // Tag to support transcript analytics feature
-
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const channelDataMiddleware = () => (next: any) => (activity: any) => {
         const applicable = activity && activity.channelData;
