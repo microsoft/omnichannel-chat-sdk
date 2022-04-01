cbd9bf73fbb4b0fa1a9a07e8b25cd5bd99ec9915
Edward Tran <edtran@microsoft.com>
2022-04-01 02:23:22 -0700

    Add tests

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,7 +1,8 @@
 const ic3ClientVersion = '2021.08.14.1';
 const webChatIC3AdapterVersion = '0.1.0-master.2dba07b';
 const webChatACSAdapterVersion = '0.0.35-beta.2';
-const ariaTelemetryKey = 'c7655518acf1403f93ff6b9f77942f0a-d01a02fd-6b50-4de3-a566-62eda11f93bc-7083';
+// const ariaTelemetryKey = 'c7655518acf1403f93ff6b9f77942f0a-d01a02fd-6b50-4de3-a566-62eda11f93bc-7083';
+const ariaTelemetryKey = '5d74ab2134c54021989ac8e1dc334ba6-263fba77-c91c-4659-a601-117a56e5e589-6508';
 
 export {
     ic3ClientVersion,
