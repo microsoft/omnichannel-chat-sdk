41e6bb3f7bfbb17816497d22fdbbb83e34600e1a
Edward Tran <edtran@microsoft.com>
2022-04-01 01:53:21 -0700

    Create CallingSDKLogger

--- src/utils/loggers.ts
+++ src/utils/loggers.ts
@@ -1,6 +1,7 @@
 import LogLevel from "../telemetry/LogLevel";
 import OmnichannelConfig from "../core/OmnichannelConfig";
 import { AWTEventData } from "../external/aria/webjs/AriaSDK";
+import ICallingSDKLogData from "../external/CallingSDK/ICallingSDKLogData";
 import IIC3SDKLogData from "../external/IC3Client/IIC3SDKLogData";
 import IOCSDKLogData from "../external/OCSDK/IOCSDKLogData";
 import AriaTelemetry from "../telemetry/AriaTelemetry";
@@ -409,6 +410,90 @@ export class ACSAdapterLogger {
     }
 }
 
+export class CallingSDKLogger {
+    private debug = false;
+    private runtimeId = '';
+    private requestId = '';
+    private chatId = '';
+    private telemetry: typeof AriaTelemetry | null = null;
+
+    constructor(private omnichannelConfig: OmnichannelConfig) {
+        this.debug = false;
+    }
+
+    /* istanbul ignore next */
+    public setDebug(flag: boolean): void {
+        this.debug = flag;
+    }
+
+    public setRuntimeId(runtimeId: string): void {
+        this.runtimeId = runtimeId;
+    }
+
+    public setRequestId(requestId: string): void {
+        this.requestId = requestId;
+    }
+
+    public setChatId(chatId: string): void {
+        this.chatId = chatId;
+    }
+
+    public useTelemetry(telemetry: typeof AriaTelemetry): void {
+        /* istanbul ignore next */
+        this.debug && console.log(`[CallingSDKLogger][useTelemetry]`);
+        this.telemetry = telemetry;
+    }
+
+    public logCallingSdkTelemetryEvent(logLevel: LogLevel, event: ICallingSDKLogData): void {
+        /* istanbul ignore next */
+        this.debug && console.log(`[CallingSDKLogger][logClientSdkTelemetryEvent][${logLevel}]`);
+        /* istanbul ignore next */
+        this.debug && console.log(event);
+
+        const baseProperties: AWTEventData["properties"] = {
+            ChatSDKRuntimeId: this.runtimeId,
+            OrgId: this.omnichannelConfig.orgId,
+            OrgUrl: this.omnichannelConfig.orgUrl,
+            WidgetId: this.omnichannelConfig.widgetId,
+            RequestId: this.requestId,
+            ChatId: this.chatId
+        };
+
+        const additionalProperties: AWTEventData["properties"] = {
+            ...event,
+            ExceptionDetails: event.ExceptionDetails? JSON.stringify(event.ExceptionDetails): '',
+        };
+
+        switch(logLevel) {
+            case LogLevel.DEBUG:
+                this.telemetry?.debug({
+                    ...baseProperties,
+                    ...additionalProperties
+                }, ScenarioType.CALLINGSDK);
+                break;
+            case LogLevel.WARN:
+                this.telemetry?.warn({
+                    ...baseProperties,
+                    ...additionalProperties
+                }, ScenarioType.CALLINGSDK);
+                break;
+            case LogLevel.ERROR:
+                this.telemetry?.error({
+                    ...baseProperties,
+                    ...additionalProperties
+                }, ScenarioType.CALLINGSDK);
+                break;
+            case LogLevel.INFO:
+            default:
+                this.telemetry?.info({
+                    ...baseProperties,
+                    ...additionalProperties
+                }, ScenarioType.CALLINGSDK);
+                break;
+        }
+    }
+}
+
 export const createIC3ClientLogger = (omnichannelConfig: OmnichannelConfig, debug = false): IC3ClientLogger => {
     const logger = new IC3ClientLogger(omnichannelConfig);
     logger.setDebug(debug);
@@ -429,6 +514,12 @@ export const createACSClientLogger = (omnichannelConfig: OmnichannelConfig, debu
 
 export const createACSAdapterLogger = (omnichannelConfig: OmnichannelConfig, debug = false): ACSAdapterLogger => {
     const logger = new ACSAdapterLogger(omnichannelConfig);
+    logger.setDebug(debug);
+    return logger;
+}
+
+export const createCallingSDKLogger = (omnichannelConfig: OmnichannelConfig, debug = false): CallingSDKLogger => {
+    const logger = new CallingSDKLogger(omnichannelConfig);
     logger.setDebug(debug);
     return logger;
 }
