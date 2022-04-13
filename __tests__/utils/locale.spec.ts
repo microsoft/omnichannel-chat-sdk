a7181e8878c4b063603ad0c011e1dcb3c913478c
Charlie Wang <charlwan@microsoft.com>
2022-04-12 18:39:28 -0700

    update locale export

--- __tests__/utils/locale.spec.ts
+++ __tests__/utils/locale.spec.ts
@@ -4,16 +4,16 @@ const locale = require('../../src/utils/locale');
 describe('Locales', () => {
   it('locale.getLocaleStringFromId() should return correctly', () => {
     expect(locale.getLocaleStringFromId("1033")).toBe("en-us");
-    expect(locale.getLocaleStringFromId(1033)).toBe("");
-    expect(locale.getLocaleStringFromId("1133")).toBe("");
+    expect(locale.getLocaleStringFromId(1033)).toBe("en-us");
+    expect(locale.getLocaleStringFromId("1133")).toBe("en-us");
     expect(locale.getLocaleStringFromId("1058")).toBe("uk-ua");
-    expect(locale.getLocaleStringFromId("")).toBe("");
+    expect(locale.getLocaleStringFromId("")).toBe("en-us");
   });
 
   it('locale.getLocaleIdFromString() should return correctly', () => {
     expect(locale.getLocaleIdFromString("en-us")).toBe("1033");
-    expect(locale.getLocaleIdFromString("en-uus")).toBe("");
-    expect(locale.getLocaleIdFromString("")).toBe("");
+    expect(locale.getLocaleIdFromString("en-uus")).toBe("1033");
+    expect(locale.getLocaleIdFromString("")).toBe("1033");
     expect(locale.getLocaleIdFromString("zh-cn")).toBe("2052");
   });
 });
