91932581fba820f332fa9a6825636fdd8ce2a2d4
xTEddie <edtran@microsoft.com>
2022-08-15 10:20:50 -0700

    Update adapter (#143)

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,6 +1,6 @@
 const ic3ClientVersion = '2021.08.14.1';
 const webChatIC3AdapterVersion = '0.1.0-master.2dba07b';
-const webChatACSAdapterVersion = '0.0.35-beta.8';
+const webChatACSAdapterVersion = '0.0.35-beta.9';
 const ariaTelemetryKey = 'c7655518acf1403f93ff6b9f77942f0a-d01a02fd-6b50-4de3-a566-62eda11f93bc-7083';
 
 export {
