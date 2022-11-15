35192fdf9a37c2bec53b3a7152047834a1ddb3ae
Edward Tran <edtran@microsoft.com>
2022-11-15 11:05:38 -0800

    Refactor

--- src/utils/urlResolvers.ts
+++ src/utils/urlResolvers.ts
@@ -15,6 +15,42 @@ const resolveIC3ClientUrl = (chatSDKConfig: ChatSDKConfig): string => {
     return libraries.getIC3ClientCDNUrl();
 };
 
+const resolveDirectLineCDNUrl = (chatSDKConfig: ChatSDKConfig) => {
+    if (chatSDKConfig.chatAdapterConfig && 'webChatDirectLineCDNUrl' in chatSDKConfig.chatAdapterConfig) {
+        return chatSDKConfig.chatAdapterConfig.webChatDirectLineCDNUrl as string;
+    }
+
+    if (chatSDKConfig.chatAdapterConfig && 'webChatDirectLineVersion' in chatSDKConfig.chatAdapterConfig) {
+        return libraries.getDirectLineCDNUrl(chatSDKConfig.chatAdapterConfig.webChatDirectLineVersion);
+    }
+
+    return libraries.getDirectLineCDNUrl();
+}
+
+const resolveACSAdapterCDNUrl = (chatSDKConfig: ChatSDKConfig) => {
+    if (chatSDKConfig.chatAdapterConfig && 'webChatACSAdapterCDNUrl' in chatSDKConfig.chatAdapterConfig) {
+        return chatSDKConfig.chatAdapterConfig.webChatACSAdapterCDNUrl as string;
+    }
+
+    if (chatSDKConfig.chatAdapterConfig && 'webChatACSAdapterVersion' in chatSDKConfig.chatAdapterConfig) {
+        return libraries.getACSAdapterCDNUrl(chatSDKConfig.chatAdapterConfig.webChatACSAdapterVersion);
+    }
+
+    return libraries.getACSAdapterCDNUrl();
+};
+
+const resolveIC3AdapterCDNUrl = (chatSDKConfig: ChatSDKConfig) => {
+    if (chatSDKConfig.chatAdapterConfig && 'webChatIC3AdapterCDNUrl' in chatSDKConfig.chatAdapterConfig) {
+        return chatSDKConfig.chatAdapterConfig.webChatIC3AdapterCDNUrl as string;
+    }
+
+    if (chatSDKConfig.chatAdapterConfig && 'webChatIC3AdapterVersion' in chatSDKConfig.chatAdapterConfig) {
+        return libraries.getIC3AdapterCDNUrl(chatSDKConfig.chatAdapterConfig.webChatIC3AdapterVersion);
+    }
+
+    return libraries.getIC3AdapterCDNUrl();
+};
+
 const resolveChatAdapterUrl = (chatSDKConfig: ChatSDKConfig, liveChatVersion: LiveChatVersion, protocol: string): string => {
     const supportedChatAdapterProtocols = [ChatAdapterProtocols.ACS, ChatAdapterProtocols.IC3, ChatAdapterProtocols.DirectLine];
     if (protocol && !supportedChatAdapterProtocols.includes(protocol as string)) {
@@ -22,39 +58,15 @@ const resolveChatAdapterUrl = (chatSDKConfig: ChatSDKConfig, liveChatVersion: Li
     }
 
     if (protocol === ChatAdapterProtocols.DirectLine) {
-        if (chatSDKConfig.chatAdapterConfig && 'webChatDirectLineCDNUrl' in chatSDKConfig.chatAdapterConfig) {
-            return chatSDKConfig.chatAdapterConfig.webChatDirectLineCDNUrl as string;
-        }
-
-        if (chatSDKConfig.chatAdapterConfig && 'webChatDirectLineVersion' in chatSDKConfig.chatAdapterConfig) {
-            return libraries.getDirectLineCDNUrl(chatSDKConfig.chatAdapterConfig.webChatDirectLineVersion);
-        }
-
-        return libraries.getDirectLineCDNUrl();
+        return resolveDirectLineCDNUrl(chatSDKConfig);
     } else if (protocol === ChatAdapterProtocols.ACS || liveChatVersion === LiveChatVersion.V2) {
-        if (chatSDKConfig.chatAdapterConfig && 'webChatACSAdapterCDNUrl' in chatSDKConfig.chatAdapterConfig) {
-            return chatSDKConfig.chatAdapterConfig.webChatACSAdapterCDNUrl as string;
-        }
-
-        if (chatSDKConfig.chatAdapterConfig && 'webChatACSAdapterVersion' in chatSDKConfig.chatAdapterConfig) {
-            return libraries.getACSAdapterCDNUrl(chatSDKConfig.chatAdapterConfig.webChatACSAdapterVersion);
-        }
-
-        return libraries.getACSAdapterCDNUrl();
+        return resolveACSAdapterCDNUrl(chatSDKConfig);
     } else if (protocol === ChatAdapterProtocols.IC3 || liveChatVersion === LiveChatVersion.V1) {
-        if (chatSDKConfig.chatAdapterConfig && 'webChatIC3AdapterCDNUrl' in chatSDKConfig.chatAdapterConfig) {
-            return chatSDKConfig.chatAdapterConfig.webChatIC3AdapterCDNUrl as string;
-        }
-
-        if (chatSDKConfig.chatAdapterConfig && 'webChatIC3AdapterVersion' in chatSDKConfig.chatAdapterConfig) {
-            return libraries.getIC3AdapterCDNUrl(chatSDKConfig.chatAdapterConfig.webChatIC3AdapterVersion);
-        }
-
-        return libraries.getIC3AdapterCDNUrl();
+        return resolveIC3AdapterCDNUrl(chatSDKConfig);
     }
 
     return '';
-}
+};
 
 export default {
     resolveIC3ClientUrl,
