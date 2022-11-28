32f58e52167001c5dffed20f2fbec8834144bafc
Edward Tran <edtran@microsoft.com>
2022-11-28 15:43:51 -0800

    Add polyfills on azure communication chat package

--- playwright/esbuild.config.js
+++ playwright/esbuild.config.js
@@ -1,6 +1,26 @@
 const { build } = require('esbuild');
 const GlobalsPolyfills = require('@esbuild-plugins/node-globals-polyfill').default;
 const NodeModulesPolyfills = require('@esbuild-plugins/node-modules-polyfill').default;
+const path = require('path');
+
+const AzureCommunicationChatPolyfills = () => {
+    return {
+        name: 'AzureCommunicationChatPolyfill',
+        setup: ({onResolve}) => {
+            onResolve({filter: /@azure\/communication-chat/}, args => {
+                // Polyfill Chat Package to support real-time notifications on Browser
+                if (args.path === '@azure/communication-chat') {
+                    const defaultPath = require.resolve('@azure/communication-chat');
+                    const basePath = path.dirname(path.dirname(defaultPath))
+                    const esmPath = path.join(basePath, 'dist-esm', 'src','index.js');
+                    return {
+                        path: esmPath.toString()
+                    }
+                }
+            })
+        }
+    }
+}
 
 build({
     entryPoints: [
@@ -9,8 +29,10 @@ build({
     bundle: true,
     outfile: 'dist/lib.js',
     format: "cjs",
+    platform: "browser",
     plugins: [
         NodeModulesPolyfills(),
-        GlobalsPolyfills()
+        GlobalsPolyfills(),
+        AzureCommunicationChatPolyfills()
     ],
 }).catch(() => process.exit(1))
