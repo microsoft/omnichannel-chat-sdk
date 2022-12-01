ab87c11f1e2eb1bbbde81fc585d9e5603a72fe6b
Edward Tran <edtran@microsoft.com>
2022-12-01 14:39:07 -0800

    Add endpoint

--- playwright/utils/OmnichannelEndpoints.ts
+++ playwright/utils/OmnichannelEndpoints.ts
@@ -4,6 +4,7 @@ export default class OmnichannelEndpoints {
     public static readonly LiveChatConfigPath = "livechatconnector/config";
     public static readonly LiveChatSessionInitPath = "livechatconnector/sessioninit";
     public static readonly LiveChatv2GetChatTokenPath = "livechatconnector/v2/getchattoken";
+    public static readonly LiveChatSessionClosePath = "livechatconnector/sessionclose";
     public static readonly SendTypingIndicatorPath = "inbound/typingindicator/livechat/sendtypingindicator";
     public static readonly LiveChatTranscriptEmailRequestPath = "livechatconnector/createemailrequest";
 }
