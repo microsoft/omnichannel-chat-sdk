338a44831a963a3ea08863abfbdf20385f727605
Edward Tran <edtran@microsoft.com>
2022-12-02 03:10:16 -0800

    Add test

--- playwright/integrations/unauthenticated-chat.spec.ts
+++ playwright/integrations/unauthenticated-chat.spec.ts
@@ -420,6 +420,62 @@ test.describe('UnauthenticatedChat @UnauthenticatedChat', () => {
         expect(runtimeContext?.errorObject).not.toBeDefined();
     });
 
+    test('ChatSDK.onNewMessage() with rehydrate flag should return previously received and new incoming messages', async ({page}) => {
+        await page.goto(testPage);
+
+        const content = "Hi";
+        const [runtimeContext] = await Promise.all([
+            await page.evaluate(async ({ omnichannelConfig, content}) => {
+                const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+                const {OmnichannelChatSDK_1: OmnichannelChatSDK} = window;
+                const chatSDK = new OmnichannelChatSDK.default(omnichannelConfig);
+
+                await chatSDK.initialize();
+
+                await chatSDK.startChat();
+
+                const runtimeContext = {
+                    requestId: chatSDK.requestId,
+                    chatId: chatSDK.chatToken.chatId,
+                    acsEndpoint: chatSDK.chatToken.acsEndpoint,
+                };
+
+                await chatSDK.sendMessage({
+                    content
+                });
+
+                const messages = [];
+
+                try {
+                    chatSDK.onNewMessage((message) => {
+                        messages.push(message);
+                    }, {rehydrate: true});
+                } catch (err) {
+                    runtimeContext.errorMessage = `${err.message}`;
+                    runtimeContext.errorObject = `${err}`;
+                }
+
+                await sleep(10000); // Wait to accumulate messages if any
+
+                runtimeContext.messages = messages;
+
+                await chatSDK.endChat();
+
+                return runtimeContext;
+            }, { omnichannelConfig, content})
+        ]);
+
+        const sentMessages = runtimeContext.messages.filter((message) => message.tags.includes('FromCustomer'));
+        const sentMessageContent = sentMessages[0].content;
+
+        expect(sentMessageContent).toBe(content);
+        expect(runtimeContext.messages).toBeDefined();
+        expect(runtimeContext.messages.length >= 0).toBe(true);
+        expect(runtimeContext?.errorMessage).not.toBeDefined();
+        expect(runtimeContext?.errorObject).not.toBeDefined();
+    });
+
     test('ChatSDK.endChat() should perform session close', async ({page}) => {
         await page.goto(testPage);
 
