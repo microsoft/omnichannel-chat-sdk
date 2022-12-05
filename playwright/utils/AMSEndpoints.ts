8bbfbff9ae113164f2c07fccb701ae05176edbd5
Edward Tran <edtran@microsoft.com>
2022-12-05 12:14:08 -0800

    Add AMSEndpoints

---
+++ playwright/utils/AMSEndpoints.ts
@@ -0,0 +1,4 @@
+export default class AMSEndpoints {
+    public static readonly rootDomain = "teams.microsoft.com";
+    public static readonly uploadImagePattern = /v1\/objects\/(.+-.+-.+-.+)\/content\/imgpsh/;
+}
