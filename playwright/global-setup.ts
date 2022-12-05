f44ba0c071e7803271bf0548c721a77f4cc8f6e8
Edward Tran <edtran@microsoft.com>
2022-12-05 11:07:40 -0800

    Add test server url as part of env config

--- playwright/global-setup.ts
+++ playwright/global-setup.ts
@@ -6,6 +6,11 @@ const globalSetup = (config: FullConfig) => {
     if (!process.env.testConfig) {
         process.env.testConfig = JSON.stringify(testConfig);
     }
+
+    // Use test server instead of static page to run tests if set
+    if (config._webServers.length > 0) {
+        process.env.testServer = config._webServers[0].url;
+    }
 }
 
 export default globalSetup;
