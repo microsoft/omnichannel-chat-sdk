feb81201c269cb1778f241690ca232ccb4c8c87b
Edward Tran <edtran@microsoft.com>
2023-01-05 00:29:07 -0800

    Add descriptive error message on script load failure

--- src/utils/WebUtils.ts
+++ src/utils/WebUtils.ts
@@ -1,4 +1,4 @@
-const loadScript = async (scriptUrl: string, callbackOnload: CallableFunction = () => void(0), callbackError: CallableFunction = () => void(0)): Promise<void> => {
+const loadScript = async (scriptUrl: string, callbackOnload: CallableFunction = () => void(0), callbackError: CallableFunction = () => void(0), retries = 0, attempt = 0): Promise<void> => {
   return new Promise (async (resolve, reject) => { // eslint-disable-line no-async-promise-executor
     const scriptElements = Array.from(document.getElementsByTagName('script'));
     const foundScriptElement = scriptElements.filter(scriptElement => scriptElement.src == scriptUrl);
@@ -19,8 +19,20 @@ const loadScript = async (scriptUrl: string, callbackOnload: CallableFunction = 
     });
 
     scriptElement.addEventListener('error', async () => {
-      await callbackError();
-      reject();
+      if (++attempt >= retries) {
+        await callbackError();
+
+        // Reference: https://developer.mozilla.org/en-US/docs/Web/API/Element/error_event
+        return reject(new Error("Resource failed to load, or can't be used."));
+      }
+
+      scriptElement.remove();
+
+      try {
+        await loadScript(scriptUrl, callbackOnload, callbackError, retries, attempt);
+      } catch (e) {
+        reject(e);
+      }
     });
   });
 };
