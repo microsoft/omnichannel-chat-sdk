0d71b115c4b827796c0e34894eb52139057561f4
Edward Tran <edtran@microsoft.com>
2023-01-05 03:39:01 -0800

    Add test

--- playwright/integrations/utilities.spec.ts
+++ playwright/integrations/utilities.spec.ts
@@ -62,4 +62,44 @@ test.describe('Utilities @Utilities', () => {
         expect(runtimeContext?.errorMessage).toBeDefined();
         expect(runtimeContext?.errorMessage).toBe(expectedErrorMessage);
     });
+
+    test('WebUtils.loadScript() retries should be using exponential backoff', async ({page}) => {
+        test.setTimeout(60 * 1000);
+
+        await page.goto(testPage);
+
+        const [runtimeContext] = await Promise.all([
+            await page.evaluate(async () => {
+                const {require_WebUtils, require_libraries} = window;
+                const WebUtils = require_WebUtils();
+                const libraries = require_libraries();
+                const scriptURL = `${libraries.getACSAdapterCDNUrl()}/invalid`;
+
+                const runtimeContext = {};
+
+                try {
+                    await WebUtils.default.loadScript(scriptURL, () => {}, () => {}, 4);
+                } catch (err) {
+                    runtimeContext.errorMessage = `${err.message}`;
+                }
+
+                runtimeContext.traces = performance.getEntries().filter((entry) => entry.initiatorType === 'script' && entry.name === scriptURL);
+
+                return runtimeContext;
+            })
+        ]);
+
+        const createDifference = (arr) => {
+            const differenceArray = [];
+            for (let i = 1; i < arr.length; i++) {
+                differenceArray.push(arr[i].startTime - arr[i - 1].startTime);
+            }
+
+            return differenceArray;
+        };
+
+        const isAscending = (arr) => arr.every((value, index) => index === 0 || value >= arr[index - 1])
+        const differenceArray = createDifference(runtimeContext.traces);
+        expect(isAscending(differenceArray)).toBe(true);
+    });
 });
