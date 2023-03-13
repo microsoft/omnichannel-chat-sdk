6a07579879b95366430d66e535c71e62fd4f1098
v-anjuvs <v-anjuvs@microsoft.com>
2023-03-13 13:10:58 +0530

    Added tests

--- playwright/integrations/authenticated-chat-with-reconnect.spec.ts
+++ playwright/integrations/authenticated-chat-with-reconnect.spec.ts
@@ -67,4 +67,89 @@ test.describe('AuthenticatedChat @AuthenticatedChatWithChatReconnect', () => {
         expect(reconnectableChatsRespense.status()).toBe(204);
         expect(reconnectableChatsRequestHeaders['authenticatedusertoken']).toBe(authToken);
     });
+
+    test('ChatSDK.getChatReconnectContext() should return a reconnect id if theres an existing chat session', async ({ page }) => {
+        await page.goto(testPage);
+
+        const [_, reconnectableChatsRequest, reconnectableChatsRespense, runtimeContext] = await Promise.all([
+            await page.evaluate(async ({ omnichannelConfig, authUrl }) => {
+                const { OmnichannelChatSDK_1: OmnichannelChatSDK } = window;
+
+                const payload = {
+                    method: "POST"
+                };
+
+                const response = await fetch(authUrl, payload);
+                const authToken = await response.text();
+
+                const chatSDKConfig = {
+                    getAuthToken: () => authToken,
+                    chatReconnect: {
+                        disable: false,
+                    },
+                };
+
+                const chatSDK = new OmnichannelChatSDK.default(omnichannelConfig, chatSDKConfig);
+
+                await chatSDK.initialize();
+
+                await chatSDK.startChat();
+
+                location.reload();
+
+            }, { omnichannelConfig, authUrl }),
+
+            page.waitForRequest(request => {
+                return request.url().includes(OmnichannelEndpoints.LiveChatAuthReconnectableChats) && request.method() === 'GET';
+            }),
+            page.waitForResponse(response => {
+                return response.url().includes(OmnichannelEndpoints.LiveChatAuthReconnectableChats) && response.request().method() === 'GET';
+            }),
+            await page.evaluate(async ({ omnichannelConfig, authUrl }) => {
+                const { OmnichannelChatSDK_1: OmnichannelChatSDK } = window;
+
+                const payload = {
+                    method: "POST"
+                };
+
+                const response = await fetch(authUrl, payload);
+                const authToken = await response.text();
+
+                const chatSDKConfig = {
+                    getAuthToken: () => authToken,
+                    chatReconnect: {
+                        disable: false,
+                    },
+                };
+
+                const chatSDK = new OmnichannelChatSDK.default(omnichannelConfig, chatSDKConfig);
+
+                await chatSDK.initialize();
+
+                const runtimeContext = {
+                    requestId: chatSDK.requestId,
+                    authToken
+                };
+
+                const chatReconnectContext = await chatSDK.getChatReconnectContext();
+
+                runtimeContext.reconnectId = chatReconnectContext.reconnectId;
+
+                await chatSDK.startChat(runtimeContext.reconnectId);
+
+                await chatSDK.endChat();
+
+                return runtimeContext;
+            }, { omnichannelConfig, authUrl })
+        ]);
+
+        const { authToken } = runtimeContext;
+        const reconnectableChatsRequestUrl = `${omnichannelConfig.orgUrl}/${OmnichannelEndpoints.LiveChatAuthReconnectableChats}/${omnichannelConfig.orgId}/${omnichannelConfig.widgetId}/${omnichannelConfig.orgId}?channelId=lcw`;
+
+        const reconnectableChatsRequestHeaders = reconnectableChatsRequest.headers();
+
+        expect(reconnectableChatsRequest.url() === reconnectableChatsRequestUrl).toBe(true);
+        expect(reconnectableChatsRespense.status()).toBe(200);
+        expect(reconnectableChatsRequestHeaders['authenticatedusertoken']).toBe(authToken);
+    });
 });
