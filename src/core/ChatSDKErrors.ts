c1961e2a4e5743e53aa2ce4bfe7024810c712536
Edward Tran <edtran@microsoft.com>
2023-03-31 15:14:24 -0700

    Add comments

--- src/core/ChatSDKErrors.ts
+++ src/core/ChatSDKErrors.ts
@@ -1,8 +1,18 @@
+/**
+ * Enum of ChatSDK standard errors.
+ * 
+ * @enum {string}
+ */
 enum ChatSDKErrors {
+    /** Failure in ChatAdapter initialization */
     ChatAdapterInitializationFailure = "ChatAdapterInitializationFailure",
+    /** Failure in loading a script dynamically */
     ScriptLoadFailure = "ScriptLoadFailure",
+    /** Feature not supported in the platform used */
     UnsupportedPlatform = "UnsupportedPlatform",
+    /** Conversation has been closed (WrapUp/Closed state) */
     ClosedConversation = "ClosedConversation",
+    /** Conversation is invalid or not found */
     InvalidConversation = "InvalidConversation"
 }
 
