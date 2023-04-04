649e705f78f3a0d8f1080f7bd355c2e73f186514
Edward Tran <edtran@microsoft.com>
2023-04-03 18:25:47 -0700

    Add documentation

--- src/core/ChatSDKExceptionDetails.ts
+++ src/core/ChatSDKExceptionDetails.ts
@@ -1,3 +1,10 @@
+/**
+ *  Interface for ChatSDK exceptions
+ *
+ *  @param response {string} Response of the exception in CamelCase. The format should be easily caught and handle in code. (e.g: StartChatFailure)
+ *  @param message {string} User friendly message. Usually used to be displayed to the users.
+ *  @param errorObject {string} Error object in string format. It's useful for investigation.
+ */
 interface ChatSDKExceptionDetails {
     response: string;
     message?: string;
