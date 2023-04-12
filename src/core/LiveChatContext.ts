5fe8fa131b734b4e9400b91f59292a63b2123b63
Edward Tran <edtran@microsoft.com>
2023-04-11 17:00:57 -0700

    Add reconnectId to live chat context

--- src/core/LiveChatContext.ts
+++ src/core/LiveChatContext.ts
@@ -1,6 +1,14 @@
 import IChatToken from "../external/IC3Adapter/IChatToken";
 
+/**
+ * Context of a live chat conversation. It contains all the necessary metadata to interact with a conversation.
+ *
+ * chatToken: Omnichannel chat metadata to interact with message/voice client
+ * requestId: Unique ID used to retrieve a conversation
+ * reconnectId: Unique ID used to retrieve conversation for chat reconnect or persistent chat tied to the user
+ */
 export default interface LiveChatContext {
     chatToken: IChatToken,
-    requestId: string
+    requestId: string,
+    reconnectId?: string
 }
