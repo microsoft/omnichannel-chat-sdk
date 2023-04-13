d3c83ee800c79dbb14714a53663301c9678fabb0
Edward Tran <edtran@microsoft.com>
2023-04-13 04:50:18 -0700

    Add integration test

--- playwright/integrations/unauthenticated-chat.spec.ts
+++ playwright/integrations/unauthenticated-chat.spec.ts
@@ -691,4 +691,69 @@ test.describe('UnauthenticatedChat @UnauthenticatedChat', () => {
         expect(liveWorkItemDetailsResponseDataJson.ConversationId).toBe(conversationDetails.conversationId);
         expect(liveWorkItemDetailsResponseDataJson.CanRenderPostChat).toBe(conversationDetails.canRenderPostChat);
     });
+
+    test('ChatSDK.getConversationDetails() with liveChatContext should not fail', async ({page}) => {
+        await page.goto(testPage);
+
+        const [invalidLiveWorkItemDetailsRequest, invalidLiveWorkItemDetailsResponse, _, liveChatContextLiveWorkItemDetailsRequest, liveChatContextLiveWorkItemDetailsResponse,  runtimeContext] = await Promise.all([
+            page.waitForRequest(request => {
+                return request.url().includes(OmnichannelEndpoints.LiveChatLiveWorkItemDetailsPath);
+            }),
+            page.waitForResponse(response => {
+                return response.url().includes(OmnichannelEndpoints.LiveChatLiveWorkItemDetailsPath);
+            }),
+            await page.evaluate(async ({ omnichannelConfig }) => {
+                const { OmnichannelChatSDK_1: OmnichannelChatSDK } = window;
+                const chatSDK = new OmnichannelChatSDK.default(omnichannelConfig);
+
+                await chatSDK.initialize();
+
+                await chatSDK.startChat();
+
+                const liveChatContext = await chatSDK.getCurrentLiveChatContext();
+
+                const runtimeContext = {
+                    liveChatContext
+                };
+
+                await chatSDK.endChat();
+                runtimeContext.invalidRequestId = chatSDK.requestId;
+
+                const invalidRequestIdConversationDetails = await chatSDK.getConversationDetails();
+                runtimeContext.invalidRequestIdConversationDetails = invalidRequestIdConversationDetails;
+
+                (window as any).runtimeContext = runtimeContext;
+            }, { omnichannelConfig }),
+            page.waitForRequest(request => {
+                return request.url().includes(OmnichannelEndpoints.LiveChatLiveWorkItemDetailsPath);
+            }),
+            page.waitForResponse(response => {
+                return response.url().includes(OmnichannelEndpoints.LiveChatLiveWorkItemDetailsPath);
+            }),
+            await page.evaluate(async ({ omnichannelConfig }) => {
+                const { OmnichannelChatSDK_1: OmnichannelChatSDK, runtimeContext } = window;
+                const chatSDK = new OmnichannelChatSDK.default(omnichannelConfig);
+
+                await chatSDK.initialize();
+
+                const liveChatContextConversationDetails = await chatSDK.getConversationDetails({liveChatContext: runtimeContext.liveChatContext});
+                runtimeContext.liveChatContextConversationDetails = liveChatContextConversationDetails;
+
+                return runtimeContext;
+            }, { omnichannelConfig }),
+        ]);
+
+        const { invalidRequestId, liveChatContext, invalidRequestIdConversationDetails, liveChatContextConversationDetails } = runtimeContext;
+        const invalidLiveWorkItemDetailsRequestUrl = `${omnichannelConfig.orgUrl}/${OmnichannelEndpoints.LiveChatLiveWorkItemDetailsPath}/${omnichannelConfig.orgId}/${omnichannelConfig.widgetId}/${invalidRequestId}?channelId=lcw`;
+        const liveChatContextLiveWorkItemDetailsRequestUrl = `${omnichannelConfig.orgUrl}/${OmnichannelEndpoints.LiveChatLiveWorkItemDetailsPath}/${omnichannelConfig.orgId}/${omnichannelConfig.widgetId}/${liveChatContext.requestId}?channelId=lcw`;
+
+        expect(invalidLiveWorkItemDetailsRequest.url() === invalidLiveWorkItemDetailsRequestUrl).toBe(true);
+        expect(invalidLiveWorkItemDetailsResponse.status()).toBe(400);
+        expect(liveChatContextLiveWorkItemDetailsRequest.url() === liveChatContextLiveWorkItemDetailsRequestUrl).toBe(true);
+        expect(liveChatContextLiveWorkItemDetailsResponse.status()).toBe(200);
+        expect(invalidRequestIdConversationDetails).toEqual({});
+        expect(liveChatContextConversationDetails.state).toBeDefined();
+        expect(liveChatContextConversationDetails.conversationId).toBeDefined();
+        expect(liveChatContextConversationDetails.canRenderPostChat).toBeDefined();
+    });
 });
