6dd8a4980db27164d2f6cbd931dfbaec8aac0ec7
Edward Tran <edtran@microsoft.com>
2023-04-12 17:26:13 -0700

    Add integration test

--- playwright/integrations/authenticated-chat-with-transcripts.spec.ts
+++ playwright/integrations/authenticated-chat-with-transcripts.spec.ts
@@ -36,7 +36,6 @@ test.describe('@AuthenticatedChat @AuthenticatedChatWithTranscripts', () => {
                 const chatSDK = new OmnichannelChatSDK.default(omnichannelConfig, chatSDKConfig);
                 const runtimeContext = {};
 
-                chatSDK.setDebug(true);
                 await chatSDK.initialize();
                 runtimeContext.authToken = authToken;
 
@@ -50,6 +49,73 @@ test.describe('@AuthenticatedChat @AuthenticatedChatWithTranscripts', () => {
                 runtimeContext.transcript = transcript;
 
                 await chatSDK.endChat();
+
+                return runtimeContext;
+            }, { omnichannelConfig, authUrl })
+        ]);
+
+        const {authToken, requestId, token, chatId, transcript} = runtimeContext;
+        const requestUrl = `${omnichannelConfig.orgUrl}/${OmnichannelEndpoints.LiveChatv2AuthGetChatTranscriptPath}/${chatId}/${requestId}?channelId=lcw`;
+        const requestHeaders = request.headers();
+
+        expect(request.url() === requestUrl).toBe(true);
+        expect(requestHeaders['authorization']).toBe(token);
+        expect(requestHeaders['organizationid']).toBe(omnichannelConfig.orgId);
+        expect(requestHeaders['widgetappid']).toBe(omnichannelConfig.widgetId);
+        expect(requestHeaders['authenticatedusertoken']).toBe(authToken);
+        expect(response.status()).toBe(200);
+        expect(Object.keys(transcript).includes('chatMessagesJson'));
+        expect(typeof transcript['chatMessagesJson']).toBe('string');
+    });
+
+    test('ChatSDK.getLiveChatTranscript() with liveChatContext should not fail', async ({ page }) => {
+        await page.goto(testPage);
+
+        page.on("console", (message) => {
+            if (message.type() === "error") {
+              console.log(message.text());
+            }
+        })
+
+        const [request, response, runtimeContext] = await Promise.all([
+            page.waitForRequest(request => {
+                return request.url().includes(OmnichannelEndpoints.LiveChatv2AuthGetChatTranscriptPath);
+            }),
+            page.waitForResponse(response => {
+                return response.url().includes(OmnichannelEndpoints.LiveChatv2AuthGetChatTranscriptPath);
+            }),
+            await page.evaluate(async ({ omnichannelConfig, authUrl }) => {
+                const {OmnichannelChatSDK_1: OmnichannelChatSDK} = window;
+
+                const payload = {
+                    method: "POST"
+                };
+
+                const response = await fetch(authUrl, payload);
+                const authToken = await response.text();
+
+                const chatSDKConfig = {
+                    getAuthToken: () => authToken
+                };
+
+                const chatSDK = new OmnichannelChatSDK.default(omnichannelConfig, chatSDKConfig);
+                const runtimeContext = {};
+
+                await chatSDK.initialize();
+                runtimeContext.authToken = authToken;
+
+                await chatSDK.startChat();
+
+                const liveChatContext = await chatSDK.getCurrentLiveChatContext();
+                runtimeContext.liveChatContext = liveChatContext;
+                runtimeContext.requestId = chatSDK.requestId;
+                runtimeContext.token = chatSDK.chatToken.token;
+                runtimeContext.chatId = chatSDK.chatToken.chatId;
+
+                await chatSDK.endChat();
+
+                const transcript = await chatSDK.getLiveChatTranscript({liveChatContext});
+                runtimeContext.transcript = transcript;
 
                 return runtimeContext;
             }, { omnichannelConfig, authUrl })
