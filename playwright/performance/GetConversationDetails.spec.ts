60f3d94bd52dd2a6ab4636f9de8ba1e17e9d348b
Vishnu Priya Vulichi <v-vvulichi@microsoft.com>
2023-06-13 12:16:08 +0530

    changes for chatSDK.getConversationDetails()  performance

--- playwright/performance/GetConversationDetails.spec.ts
+++ playwright/performance/GetConversationDetails.spec.ts
@@ -7,12 +7,12 @@ const testPage = fetchTestPageUrl();
 const omnichannelConfig = fetchOmnichannelConfig('UnauthenticatedChat');
 
 test.describe('Performance @Performance ', () => {
-    test('ChatSDK.getCurrentLiveChatContext()', async ({ page }) => {
+    test('ChatSDK.getConversationDetails()', async ({ page }) => {
         await page.goto(testPage);
-
+        
         let [response, runtimeContext ] = await Promise.all([
             page.waitForResponse(response => {
-                return response.url().includes(OmnichannelEndpoints.LiveChatSessionInitPath);
+                return response.url().includes(OmnichannelEndpoints.LiveChatLiveWorkItemDetailsPath);
             }),
             await page.evaluate(async ({ omnichannelConfig }) => {
                 const { OmnichannelChatSDK_1: OmnichannelChatSDK } = window;
@@ -21,23 +21,27 @@ test.describe('Performance @Performance ', () => {
                 await chatSDK.initialize();
 
                 await chatSDK.startChat();
-
+                
                 let startTime = new Date();
-                await chatSDK.getCurrentLiveChatContext();
+                const conversationDetails = await chatSDK.getConversationDetails();
                 let endTime = new Date();
                 let timeTaken = endTime.getTime() - startTime.getTime();
 
                 const runtimeContext = {
+                    conversationDetails,
                     timeTaken: timeTaken
                 };
 
                 await chatSDK.endChat();
-                
+
                 return runtimeContext;
             }, { omnichannelConfig })
         ]);
 
-        console.log("chatSDK.getCurrentLiveChatContext(): " + runtimeContext.timeTaken);
+        console.log("chatSDK.getConversationDetails(): " + runtimeContext.timeTaken);
+        
+        const conversationDetails = runtimeContext.conversationDetails;
         expect(response.status()).toBe(200);
+        expect((await response.json()).ConversationId).toBe(conversationDetails.conversationId);
     });
 });
