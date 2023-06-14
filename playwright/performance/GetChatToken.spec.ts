4b449673614b6b7171f198a108dda1b19b0f1b8f
v-anjuvs <v-anjuvs@microsoft.com>
2023-06-14 13:26:49 +0530

    Added tests

---
+++ playwright/performance/GetChatToken.spec.ts
@@ -0,0 +1,45 @@
+import OmnichannelEndpoints from '../utils/OmnichannelEndpoints';
+import fetchOmnichannelConfig from '../utils/fetchOmnichannelConfig';
+import fetchTestPageUrl from '../utils/fetchTestPageUrl';
+import { test, expect } from '@playwright/test';
+
+const testPage = fetchTestPageUrl();
+const omnichannelConfig = fetchOmnichannelConfig('UnauthenticatedChat');
+
+test.describe('Performance @Performance', () => {
+    test('ChatSDK.getChatToken()', async ({ page }) => {
+        await page.goto(testPage);
+
+        const [sessionInitResponse, runtimeContext] = await Promise.all([
+            page.waitForResponse(response => {
+                return response.url().includes(OmnichannelEndpoints.LiveChatSessionInitPath);
+            }),
+            await page.evaluate(async ({ omnichannelConfig }) => {
+                const { OmnichannelChatSDK_1: OmnichannelChatSDK } = window;
+                const chatSDK = new OmnichannelChatSDK.default(omnichannelConfig);
+
+                await chatSDK.initialize();
+
+                await chatSDK.startChat();
+
+                let startTime = new Date();
+                await chatSDK.getChatToken();
+                let endTime = new Date();
+                let timeTaken = endTime.getTime() - startTime.getTime();
+
+                const runtimeContext = {
+                    timeTaken: timeTaken
+                };
+
+                await chatSDK.endChat();
+
+                return runtimeContext;
+            }, { omnichannelConfig })
+        ]);
+
+        const { timeTaken } = runtimeContext;
+        console.log("chatSDK.getChatToken(): " + timeTaken);
+
+        expect(sessionInitResponse.status()).toBe(200);
+    });
+});
