26d4011776e5d8ca5b3e8386d833bf8c35efd7c7
Vishnu Priya Vulichi <v-vvulichi@microsoft.com>
2023-06-21 12:54:58 +0530

    changes for getDataMaskingRules() and getPostChatSurveyContext() performance

---
+++ playwright/performance/GetDataMaskingRules.spec.ts
@@ -0,0 +1,42 @@
+import fetchOmnichannelConfig from '../utils/fetchOmnichannelConfig';
+import fetchTestPageUrl from '../utils/fetchTestPageUrl';
+import { test, expect } from '@playwright/test';
+import OmnichannelEndpoints from '../utils/OmnichannelEndpoints';
+
+const testPage = fetchTestPageUrl();
+const omnichannelConfig = fetchOmnichannelConfig('UnauthenticatedChat');
+
+test.describe('Performance @Performance ', () => {
+    test('ChatSDK.getDataMaskingRules()', async ({ page }) => {
+        await page.goto(testPage);
+
+        const dataMaskingRuleJson = JSON.stringify("\\b(?!000|666|9)\\d{3}[- ]?(?!00)\\d{2}[- ]?(?!0000)\\d{4}\\b");
+
+        let [runtimeContext ] = await Promise.all([
+            await page.evaluate(async ({ omnichannelConfig }) => {
+                const { OmnichannelChatSDK_1: OmnichannelChatSDK } = window;
+                let chatSDK = new OmnichannelChatSDK.default(omnichannelConfig);
+
+                await chatSDK.initialize();
+
+                let startTime = new Date();
+                const maskingRules = await chatSDK.getDataMaskingRules();
+                let endTime = new Date();
+                let timeTaken = endTime.getTime() - startTime.getTime();
+
+                const runtimeContext = {
+                    requestId: chatSDK.requestId,
+                    timeTaken: timeTaken,
+                    maskingRules: maskingRules
+                };
+                
+                return runtimeContext;
+            }, { omnichannelConfig }),
+        ]);
+
+        console.log("chatSDK.getDataMaskingRules(): " + runtimeContext.timeTaken);
+        expect(runtimeContext.maskingRules).not.toBeNull();
+        const maskingRulesJson = JSON.stringify(runtimeContext.maskingRules)
+        expect(maskingRulesJson.includes(dataMaskingRuleJson)).toBeTruthy();
+    });
+});
---
+++ playwright/performance/GetPostChatSurveyContext.spec.ts
@@ -0,0 +1,42 @@
+import fetchOmnichannelConfig from '../utils/fetchOmnichannelConfig';
+import fetchTestPageUrl from '../utils/fetchTestPageUrl';
+import { test, expect } from '@playwright/test';
+
+const testPage = fetchTestPageUrl();
+const omnichannelConfig = fetchOmnichannelConfig('PostChatSurvey');
+
+test.describe('Performance @Performance ', () => {
+    test('ChatSDK.getPostChatSurveyContext()', async ({ page }) => {
+        await page.goto(testPage);
+
+        let [ runtimeContext ] = await Promise.all([
+            await page.evaluate(async ({ omnichannelConfig }) => {
+                const { OmnichannelChatSDK_1: OmnichannelChatSDK } = window;
+                let chatSDK = new OmnichannelChatSDK.default(omnichannelConfig);
+
+                await chatSDK.initialize();
+
+                await chatSDK.startChat();
+
+                let startTime = new Date();
+                const postChatSurveyContext = await chatSDK.getPostChatSurveyContext();
+                let endTime = new Date();
+                let timeTaken = endTime.getTime() - startTime.getTime();
+
+                const runtimeContext = {
+                    requestId: chatSDK.requestId,
+                    timeTaken: timeTaken,
+                    postChatSurveyContext: postChatSurveyContext
+                };
+
+                await chatSDK.endChat();
+                
+                return runtimeContext;
+            }, { omnichannelConfig }),
+        ]);
+
+        console.log("chatSDK.getPostChatSurveyContext(): " + runtimeContext.timeTaken);
+        expect(runtimeContext.postChatSurveyContext).not.toBeNull();
+        expect(runtimeContext.postChatSurveyContext.participantType).toBe("Bot");
+    });
+});
