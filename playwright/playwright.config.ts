898164ef82780a9dd61e6b22a516c51960d3f0d6
v-anjuvs <v-anjuvs@microsoft.com>
2023-09-19 16:42:59 +0530

    Added changes

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -1,5 +1,8 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
+import path from "path";
+const outPutDir = path.join(".", "generated/tests");
+const jUnitTestResults = path.join(outPutDir, "test-results/test-results.xml");
 
 const config: PlaywrightTestConfig = {
   globalSetup: require.resolve('./global-setup'),
@@ -16,17 +19,21 @@ const config: PlaywrightTestConfig = {
   expect: {
     timeout: 5000
   },
+  outputDir: process.env.OUTPUTDIR || outPutDir,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : 1,
   reporter: [
-    ['html', {outputFolder: 'integrations-report'}]
+    ['html', {outputFolder: 'integrations-report'}],
+    ["junit", { outputFile: jUnitTestResults }]
   ],
   use: {
     headless: true,
     actionTimeout: 0,
     trace: 'on-first-retry',
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
     launchOptions: {
       slowMo: parseInt(process.env.PLAYWRIGHT_SLOW_MO || 1000)
     }
