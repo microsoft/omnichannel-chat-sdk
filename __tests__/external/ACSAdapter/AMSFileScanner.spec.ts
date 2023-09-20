5b5b05a642653ffdb57668ae3d64ac24c607666d
Edward Tran <edtran@microsoft.com>
2023-09-19 23:05:12 -0700

    Add test

--- __tests__/external/ACSAdapter/AMSFileScanner.spec.ts
+++ __tests__/external/ACSAdapter/AMSFileScanner.spec.ts
@@ -78,4 +78,26 @@ describe("AMSFileScanner", () => {
         expect(initialScanResult).toEqual({fileMetadata: sampleFileMetadata, scan: sampleScanResponse});
         expect(newScanResult).toEqual({fileMetadata: sampleFileMetadata, scan: newSampleScanResponse});
     });
+
+    it("AMSFileScanner.addNext() should fill the next property of the scan result", async () => {
+        (global as any).setTimeout = jest.fn();
+        const amsClient: any = {};
+        const fileScanner = new AMSFileScanner(amsClient);
+
+        fileScanner.scanFiles = jest.fn();
+
+        const sampleFileId = "fileId";
+        const sampleScanResult = {fileMetadata: {id: "id", type: "type"}, scan: {status: "status"}};
+
+        fileScanner.scanResults?.set(sampleFileId, sampleScanResult);
+
+        const next = jest.fn();
+        fileScanner.addNext(sampleFileId, next);
+
+        const scanResult = fileScanner.scanResults?.get(sampleFileId);
+
+        expect(fileScanner.scanResults?.size === 1).toBe(true);
+        expect(scanResult).toEqual({...sampleScanResult, next});
+        expect(scanResult?.next).toEqual(next);
+    });
 });
