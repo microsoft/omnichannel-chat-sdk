f8d8471e944b9402fd16b26515a53ce80320025a
Edward Tran <edtran@microsoft.com>
2023-09-19 22:38:20 -0700

    Add test

--- __tests__/external/ACSAdapter/AMSFileScanner.spec.ts
+++ __tests__/external/ACSAdapter/AMSFileScanner.spec.ts
@@ -2,12 +2,24 @@ import AMSFileScanner from '../../../src/external/ACSAdapter/AMSFileScanner';
 
 describe("AMSFileScanner", () => {
 
-    it("AMSFileManager initialization should call setTimeout()", () => {
+    it("AMSFileScanner initialization should call setTimeout()", () => {
         (global as any).setTimeout = jest.fn();
 
         const amsClient: any = {};
         new AMSFileScanner(amsClient);
 
         expect((global as any).setTimeout).toHaveBeenCalled();
+    });
+
+    it("AMSFileScanner.queueScan() should call AMSFileScanner.scanFiles()", async () => {
+        (global as any).setTimeout = jest.fn();
+        const amsClient: any = {}; // eslint-disable-line @typescript-eslint/no-explicit-any
+        const fileScanner = new AMSFileScanner(amsClient);
+
+        fileScanner.scanFiles = jest.fn();
+        fileScanner.queueScan();
+
+        expect((global as any).setTimeout).toHaveBeenCalled();
+        expect(fileScanner.scanFiles).toHaveBeenCalledTimes(1);
     });
 });
