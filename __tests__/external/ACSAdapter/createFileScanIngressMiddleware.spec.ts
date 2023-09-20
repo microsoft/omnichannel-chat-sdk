6a93b53907a1b494e8931aa9262040dfddead3b8
Edward Tran <edtran@microsoft.com>
2023-09-20 04:58:29 -0700

    Add test

--- __tests__/external/ACSAdapter/createFileScanIngressMiddleware.spec.ts
+++ __tests__/external/ACSAdapter/createFileScanIngressMiddleware.spec.ts
@@ -9,4 +9,27 @@ describe('createFileScanIngressMiddleware', () => {
 
         expect(next).toHaveBeenCalledWith(activity);
     });
+
+    it("createFileScanIngressMiddleware should populate 'activity.channelData.metadata.amsreferences' from 'activity.channelData.metadata.amsReferences' if doesn't exist", () => {
+        const next = jest.fn();
+
+        const fileMetadata = {id: "id", type: "type", name: "name", size: 0};
+        const attachment = {contentType: fileMetadata.type, name: fileMetadata.name, thumbnailUrl: undefined};
+        const attachments = [attachment];
+        const amsReferences = [fileMetadata.id];
+        const activity = {
+            attachments,
+            channelData: {
+                metadata: {
+                    amsReferences: JSON.stringify(amsReferences)
+                }
+            }
+        };
+
+        createFileScanIngressMiddleware()()(next)(activity);
+
+        expect(next).toHaveBeenCalledWith(activity);
+        expect(Object.keys(activity.channelData.metadata).indexOf("amsreferences") >= 0).toBe(true);
+        expect((activity.channelData.metadata as any)["amsreferences"]).toBe(activity.channelData.metadata.amsReferences);
+    });
 });
