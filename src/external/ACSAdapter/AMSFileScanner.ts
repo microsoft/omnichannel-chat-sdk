f46f823626440d68cad04539bf55fa184ff3e090
Edward Tran <edtran@microsoft.com>
2023-09-20 03:40:26 -0700

    Refactor

--- src/external/ACSAdapter/AMSFileScanner.ts
+++ src/external/ACSAdapter/AMSFileScanner.ts
@@ -63,62 +63,62 @@ class AMSFileScanner {
         }
     }
 
-    public scanFiles(): Promise<void> {
-        const scan = async (scanResult: FileScanResult, id: string) => {
-            const {fileMetadata, next, activity} = scanResult;
+    public async scanFileCallback(scanResult: FileScanResult, id: string): Promise<void> {
+        const {fileMetadata, next, activity} = scanResult;
 
-            if (scanResult?.scan?.status === AMSDownloadStatus.INPROGRESS) {
-                try {
-                    const response: any = await this.amsClient.getViewStatus(fileMetadata); // eslint-disable-line @typescript-eslint/no-explicit-any
-                    const {view_location, scan} = response;
+        if (scanResult?.scan?.status === AMSDownloadStatus.INPROGRESS) {
+            try {
+                const response: any = await this.amsClient.getViewStatus(fileMetadata); // eslint-disable-line @typescript-eslint/no-explicit-any
+                const {view_location, scan} = response;
 
-                    this.addOrUpdateFile(id, scanResult.fileMetadata, scan);
+                this.addOrUpdateFile(id, scanResult.fileMetadata, scan);
 
-                    if (scan.status === AMSDownloadStatus.PASSED && next && activity) {
-                        let blob: any; // eslint-disable-line @typescript-eslint/no-explicit-any
-                        try {
-                            blob = await this.amsClient.getView(fileMetadata, view_location); // eslint-disable-line @typescript-eslint/no-explicit-any
-                        } catch (error) {
-                            console.error(error);
-                        }
-
-                        const file = new File([blob], fileMetadata.name as string, { type: fileMetadata.type});
-                        const attachmentData = await activityUtils.getAttachments([file]);
-                        const attachmentSizes = await activityUtils.getAttachmentSizes([file]);
-
-                        const index = activity.attachments.findIndex((attachment: any) => (attachment.name === fileMetadata.name)); // eslint-disable-line @typescript-eslint/no-explicit-any
-                        activity.channelData.fileScan[index] = scan;
-                        activity.attachments[index] = attachmentData[0];
-                        activity.channelData.attachmentSizes[index] = attachmentSizes[0];
-
-                        const hasMultipleAttachments = index > 0;
-                        if (hasMultipleAttachments) {
-                            const {metadata: {amsreferences}} = activity.channelData;
-                            const fileIds = JSON.parse(amsreferences);
-                            fileIds.forEach((fileId: string) => {
-                                this.addActivity(fileId, activity);
-                            });
-                        }
-
-                        next(activity); // Send updated activity to webchat
+                if (scan.status === AMSDownloadStatus.PASSED && next && activity) {
+                    let blob: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+                    try {
+                        blob = await this.amsClient.getView(fileMetadata, view_location); // eslint-disable-line @typescript-eslint/no-explicit-any
+                    } catch (error) {
+                        console.error(error);
                     }
 
-                    if (scan.status === AMSDownloadStatus.MALWARE && next && activity) {
-                        const index = activity.attachments.findIndex((attachment: any) => (attachment.name === fileMetadata.name)); // eslint-disable-line @typescript-eslint/no-explicit-any
-                        activity.channelData.fileScan[index] = scan;
-                        next(activity);
+                    const file = new File([blob], fileMetadata.name as string, { type: fileMetadata.type});
+                    const attachmentData = await activityUtils.getAttachments([file]);
+                    const attachmentSizes = await activityUtils.getAttachmentSizes([file]);
+
+                    const index = activity.attachments.findIndex((attachment: any) => (attachment.name === fileMetadata.name)); // eslint-disable-line @typescript-eslint/no-explicit-any
+                    activity.channelData.fileScan[index] = scan;
+                    activity.attachments[index] = attachmentData[0];
+                    activity.channelData.attachmentSizes[index] = attachmentSizes[0];
+
+                    const hasMultipleAttachments = index > 0;
+                    if (hasMultipleAttachments) {
+                        const {metadata: {amsreferences}} = activity.channelData;
+                        const fileIds = JSON.parse(amsreferences);
+                        fileIds.forEach((fileId: string) => {
+                            this.addActivity(fileId, activity);
+                        });
                     }
-                } catch (e) {
-                    console.error(e);
+
+                    next(activity); // Send updated activity to webchat
                 }
+
+                if (scan.status === AMSDownloadStatus.MALWARE && next && activity) {
+                    const index = activity.attachments.findIndex((attachment: any) => (attachment.name === fileMetadata.name)); // eslint-disable-line @typescript-eslint/no-explicit-any
+                    activity.channelData.fileScan[index] = scan;
+                    next(activity);
+                }
+            } catch (e) {
+                console.error(e);
             }
         }
+    }
 
-        this.scanResults?.forEach(async (scanResult, id) => {await scan(scanResult, id)});
+    public scanFiles(): Promise<void> {
+        this.scanResults?.forEach(async (scanResult, id) => {await this.scanFileCallback(scanResult, id)});
 
         // eslint-disable-next-line no-async-promise-executor
         return new Promise(async (resolve) => {
-            this.scanResults?.forEach(async (scanResult, id) => {await scan(scanResult, id)});
+            this.scanResults?.forEach(async (scanResult, id) => {await this.scanFileCallback(scanResult, id)});
             await sleep(1000);
             resolve();
         });
