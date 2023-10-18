1ac79cd70c2a1eb90aee722acdf7fbc365eacd45
Edward Tran <edtran@microsoft.com>
2023-10-18 12:28:09 -0700

    Add EUDomainNames

---
+++ src/telemetry/EUDomainNames.ts
@@ -0,0 +1,9 @@
+const EUDomainNames = [
+    "crm4.omnichannelengagementhub.com", // EUR
+    "crm12.omnichannelengagementhub.com", // FRA
+    "crm16.omnichannelengagementhub.com", // GER
+    "crm17.omnichannelengagementhub.com", // CHE
+    "crm19.omnichannelengagementhub.com" // NOR
+];
+
+export default EUDomainNames;
