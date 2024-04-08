de6cbf61fcc56b29e3306cf322a6442fe3e7395d
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-04-08 13:15:05 -0700

    Update fallback to be performed on all orgs

--- src/utils/internalUtils.ts
+++ src/utils/internalUtils.ts
@@ -2,13 +2,12 @@
  * Utility function that are only meant to be used internally. It would not guarantee backward compatibility if they were used outside of the package.
  */
 
-import { nonProductionDynamicsLocationCode } from "./CoreServicesUtils";
 
 export const isCoreServicesOrgUrlDNSError = (error: any, coreServicesOrgUrl: string | null, dynamicsLocationCode: string | null) => {
     // Validating whether it's an 'ERR_NAME_NOT_RESOLVED' error
     // `ERR_NETWORK` could return false positives since the error can be caused by network disconnection
     const isDNSUrlResolveError = error.isAxiosError && error.code == AxiosErrorCodes.ERR_NETWORK;
-    if (isDNSUrlResolveError && coreServicesOrgUrl && dynamicsLocationCode && nonProductionDynamicsLocationCode.includes(dynamicsLocationCode)) { // eslint-disable-line @typescript-eslint/no-explicit-any
+    if (isDNSUrlResolveError && coreServicesOrgUrl && dynamicsLocationCode) { // eslint-disable-line @typescript-eslint/no-explicit-any
         return true;
     }
     
