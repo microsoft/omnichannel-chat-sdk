f0528f4b892b008cbe237aa7fe3d40a1f4f7e738
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-04-09 16:10:58 -0700

    Fix lint

--- src/utils/internalUtils.ts
+++ src/utils/internalUtils.ts
@@ -4,7 +4,7 @@
 
 import AxiosErrorCodes from "../core/AxiosErrorCodes";
 
-export const isCoreServicesOrgUrlDNSError = (error: any, coreServicesOrgUrl: string | null, dynamicsLocationCode: string | null): boolean => { // eslint-disable-line @typescript-eslint/no-explicit-any
+export const isCoreServicesOrgUrlDNSError = (error: any, coreServicesOrgUrl: string | null, dynamicsLocationCode: string | null): boolean => { // eslint-disable-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
     // Validating whether it's an 'ERR_NAME_NOT_RESOLVED' error
     // `ERR_NETWORK` could return false positives since the error can be caused by network disconnection
     const isDNSUrlResolveError = error.isAxiosError && error.code == AxiosErrorCodes.ERR_NETWORK;
