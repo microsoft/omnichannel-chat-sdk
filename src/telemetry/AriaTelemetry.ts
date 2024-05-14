f2cc9b6b202f5827668d3168855262dbca36afdc
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-05-14 16:20:30 -0700

    Log 'ACSSignaling' version as part of NPMPackagesInfo

--- src/telemetry/AriaTelemetry.ts
+++ src/telemetry/AriaTelemetry.ts
@@ -28,6 +28,7 @@ interface NPMPackagesInfo {
     IC3Core?: string;
     ACSChat?: string;
     ACSCommon?: string;
+    ACSSignaling?: string;
     AMSClient?: string;
 }
 
@@ -672,6 +673,7 @@ class AriaTelemetry {
             IC3Core: require('@microsoft/omnichannel-ic3core/package.json').version, // eslint-disable-line @typescript-eslint/no-var-requires
             ACSChat: require('@azure/communication-chat/package.json').version, // eslint-disable-line @typescript-eslint/no-var-requires
             ACSCommon: require('@azure/communication-common/package.json').version, // eslint-disable-line @typescript-eslint/no-var-requires
+            ACSSignaling: require('@azure/communication-signaling').version, // eslint-disable-line @typescript-eslint/no-var-requires
             AMSClient:  require('@microsoft/omnichannel-amsclient/package.json').version, // eslint-disable-line @typescript-eslint/no-var-requires
         };
 
