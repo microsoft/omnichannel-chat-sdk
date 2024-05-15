f1824e999344cf181aa12634aaf7aaab9d840f3a
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-05-15 09:29:26 -0700

    Fix lint

--- src/utils/setOcUserAgent.ts
+++ src/utils/setOcUserAgent.ts
@@ -1,5 +1,5 @@
-const setOcUserAgent = (OCClient: any, ocUserAgent?: string[]) => {
-    const version = require('../../package.json').version;
+const setOcUserAgent = (OCClient: any, ocUserAgent?: string[]): void => { // eslint-disable-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types 
+    const version = require('../../package.json').version; // eslint-disable-line @typescript-eslint/no-var-requires
     const userAgent = `omnichannel-chat-sdk/${version}`;
     OCClient.ocUserAgent = [userAgent, ...OCClient.ocUserAgent];
 
