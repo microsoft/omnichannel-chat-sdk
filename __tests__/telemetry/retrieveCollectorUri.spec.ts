27cff585664a551d86268c595c0afa10eb2186bd
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-07-02 11:49:29 -0700

    Add tests

--- __tests__/telemetry/retrieveCollectorUri.spec.ts
+++ __tests__/telemetry/retrieveCollectorUri.spec.ts
@@ -1,9 +1,11 @@
 import retrieveCollectorUri from '../../src/telemetry/retrieveCollectorUri';
 import EUDomainNames from '../../src/telemetry/EUDomainNames';
+import GCCDomainPatterns from '../../src/telemetry/GCCDomainPatterns';
 
 describe('retrieveCollectorUri', () => {
     const defaultCollectorUri = "https://browser.pipe.aria.microsoft.com/Collector/3.0/";
     const EUCollectorUri = "https://eu-mobile.events.data.microsoft.com/Collector/3.0/";
+    const GCCCollectorUri = "https://tb.pipe.aria.microsoft.com/Collector/3.0/";
 
     it('retrieveCollectorUri() should return default EUCollectorUri on EU orgUrls', () => {
         for (const domain of EUDomainNames) {
@@ -17,6 +19,22 @@ describe('retrieveCollectorUri', () => {
         const orgUrl = "microsoft.com";
         const collectorUri = retrieveCollectorUri(orgUrl);
 
+        expect(collectorUri).toBe(defaultCollectorUri);
+    });
+
+    it('retrieveCollectorUri() should return default GCCCollectorUri on GCC orgUrl', () => {
+        for (const domainPattern of GCCDomainPatterns) {
+            const orgUrl = `foo.${domainPattern}.bar`;
+            const collectorUri = retrieveCollectorUri(orgUrl);
+            expect(collectorUri).toBe(GCCCollectorUri);
+        }
+    });
+
+    it('retrieveCollectorUri() with \'crm9\' as part of the org name should NOT return GCCCollectorUri on GCC orgUrl', () => {
+        const orgUrl = `[prefix]crm9[suffix].crm90.bar`;
+        const collectorUri = retrieveCollectorUri(orgUrl);
+
+        expect(collectorUri).not.toBe(GCCCollectorUri);
         expect(collectorUri).toBe(defaultCollectorUri);
     });
 });
