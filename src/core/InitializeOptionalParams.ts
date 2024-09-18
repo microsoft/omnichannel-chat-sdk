9d408f06c7814107d77385e2691960d83ac74f7d
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-09-18 14:57:14 -0700

    Add property to use concurrency

--- src/core/InitializeOptionalParams.ts
+++ src/core/InitializeOptionalParams.ts
@@ -2,6 +2,7 @@ import GetLiveChatConfigOptionalParams from "./GetLiveChatConfigOptionalParams"
 
 interface InitializeOptionalParams {
     getLiveChatConfigOptionalParams?: GetLiveChatConfigOptionalParams;
+    useConcurrency?: boolean;
 }
 
 export default InitializeOptionalParams
