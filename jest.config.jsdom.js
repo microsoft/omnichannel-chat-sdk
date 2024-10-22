0e171fc35616fc684cf17a955081051e68a8162e
Edgar Lopez <elopezanaya@gmail.com>
2024-10-21 18:21:52 -0700

    ok, wip

--- jest.config.jsdom.js
+++ jest.config.jsdom.js
@@ -5,6 +5,10 @@ module.exports = {
     "<rootDir>/samples/",
     "<rootDir>/playwright/"
   ],
+  // add matched test for all tests in the project that contains the word .web.spec.ts in the name
+  testMatch: [
+    "**/*.web.spec.ts"
+  ],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
     "^.+\\.jsx?$": "babel-jest"
--- jest.config.node.js
+++ jest.config.node.js
@@ -1,6 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  //add matched test for all tests in the project that contains the word .node.spec.ts in the name and it doesnt contain web.spec.ts
+  testMatch: [
+    "**/*.node.spec.ts"
+  ],
   testPathIgnorePatterns : [
     "<rootDir>/samples/",
     "<rootDir>/playwright/"
