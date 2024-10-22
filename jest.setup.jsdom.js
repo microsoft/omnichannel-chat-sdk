8560e81ac09610ee4db51b79a5027baed8a693d9
Edgar Lopez <elopezanaya@gmail.com>
2024-10-22 06:25:24 -0700

    ok, at this point all web tests are passing

--- jest.setup.jsdom.js
+++ jest.setup.jsdom.js
@@ -1,22 +1,19 @@
-const { JSDOM } = require("jsdom");
-const { TextEncoder, TextDecoder } = require("util");
+const { TextEncoder: PolyfillTextEncoder, TextDecoder: PolyfillTextDecoder } = require("text-encoding");
+// Use polyfill TextEncoder/TextDecoder
+global.TextEncoder = PolyfillTextEncoder;
+global.TextDecoder = PolyfillTextDecoder;
 
 if (global.window === undefined) {
+    const { JSDOM } = require("jsdom");
     const jsdom = new JSDOM();
-
-     const { window } = jsdom;
-
-     global.window = window;
-
+    const { window } = jsdom;
+    global.window = window;
     global.document = window.document;
     global.navigator = window.navigator;
     global.DOMParser = window.DOMParser;
     global.Node = window.Node;
     global.XMLSerializer = window.XMLSerializer;
-    global.TextEncoder = TextEncoder;
-    global.TextDecoder = TextDecoder;
     global.window.dispatchEvent = ()=> {}
-
 }
 
 global.self = global; // Mock the `self` global variable
