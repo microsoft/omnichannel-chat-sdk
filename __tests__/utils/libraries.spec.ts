23d5c177faee43957bc310bb0ad1f63d614559e7
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-11-01 15:53:39 -0700

    Remove unused v1 code paths

--- __tests__/utils/libraries.spec.ts
+++ __tests__/utils/libraries.spec.ts
@@ -1,18 +1,6 @@
-import { getACSAdapterCDNUrl, getIC3AdapterCDNUrl, getIC3ClientCDNUrl } from "../../src/utils/libraries";
+import { getACSAdapterCDNUrl } from "../../src/utils/libraries";
 
 describe('Libraries', () => {
-    it('IC3Client should come from a production URL', () => {
-        const productionUrl = "https://comms.omnichannelengagementhub.com/release";
-        const ic3ClientCDNurl = getIC3ClientCDNUrl();
-        expect(ic3ClientCDNurl.includes(productionUrl)).toBe(true);
-    });
-
-    it('WebChatIC3Adapter should come from a production URL', () => {
-        const productionUrl = "https://webchatic3.blob.core.windows.net/webchat-ic3adapter";
-        const ic3AdapterCDNUrl = getIC3AdapterCDNUrl();
-        expect(ic3AdapterCDNUrl.includes(productionUrl)).toBe(true);
-    });
-
     it('WebChatACSAdapter should come from a production URL', () => {
         const productionUrl = "https://unpkg.com/acs_webchat-chat-adapter";
         const acsAdapterCDNUrl = getACSAdapterCDNUrl();
--- src/utils/libraries.ts
+++ src/utils/libraries.ts
@@ -1,9 +1,4 @@
-import { ic3ClientVersion, webChatACSAdapterVersion, webChatDirectLineVersion, webChatIC3AdapterVersion } from "../config/settings";
-
-const getIC3ClientCDNUrl = (version = ic3ClientVersion): string => {
-    const IC3ClientCDNUrl = `https://comms.omnichannelengagementhub.com/release/${version}/Scripts/SDK/SDK.min.js`;
-    return IC3ClientCDNUrl;
-}
+import { webChatACSAdapterVersion, webChatDirectLineVersion, webChatIC3AdapterVersion } from "../config/settings";
 
 const getIC3AdapterCDNUrl = (version = webChatIC3AdapterVersion): string => {
     const IC3AdapterCDNUrl = `https://webchatic3.blob.core.windows.net/webchat-ic3adapter/${version}/botframework-webchat-adapter-ic3.production.min.js`;
@@ -21,14 +16,12 @@ const getDirectLineCDNUrl = (version = webChatDirectLineVersion): string => {
 }
 
 export default {
-    getIC3ClientCDNUrl,
     getIC3AdapterCDNUrl,
     getACSAdapterCDNUrl,
     getDirectLineCDNUrl
 }
 
 export {
-    getIC3ClientCDNUrl,
     getIC3AdapterCDNUrl,
     getACSAdapterCDNUrl,
     getDirectLineCDNUrl
