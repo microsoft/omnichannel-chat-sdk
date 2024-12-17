92dc9967a9d3c0ff5907b8700fd5fa01ff14c90d
Edgar Lopez <elopezanaya@gmail.com>
2024-12-17 15:59:19 -0800

    change type

--- src/types/response.d.ts
+++ src/types/response.d.ts
@@ -9,6 +9,7 @@ export type MaskingRules = {
 }
 export type GeneralResponse = {} | string | undefined;
 export type GetPreChatSurveyResponse = GeneralResponse;
+export type GetLiveChatTranscriptResponse = GeneralResponse;
 export type DirectLineAdapter = ReturnType<typeof createDirectLine>;
 export type ACSAdapter = ReturnType<typeof createACSAdapter>;
 export type IC3Adapter = ReturnType<typeof createIC3Adapter>;
