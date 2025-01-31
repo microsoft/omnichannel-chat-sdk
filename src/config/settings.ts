84d601738c635a7575b4b9fe3cf31ec947638f13
Emlyn Bolton <3941071+emlynmac@users.noreply.github.com>
2025-01-31 12:30:17 -0800

    Update package version

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,6 +1,6 @@
 const ic3ClientVersion = '2021.08.14.1';
 const webChatIC3AdapterVersion = '0.1.0-master.2dba07b';
-const webChatACSAdapterVersion = '0.0.35-beta.20';
+const webChatACSAdapterVersion = '0.0.1-beta.1';
 const webChatDirectLineVersion = '0.15.1';
 const callingBundleVersion = '1.0.20240809-2';
 const ariaTelemetryKey = 'c7655518acf1403f93ff6b9f77942f0a-d01a02fd-6b50-4de3-a566-62eda11f93bc-7083';
