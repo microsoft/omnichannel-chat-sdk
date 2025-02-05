c1cd40730772f3a371a407269dea283263054d36
Edgar Lopez <elopezanaya@gmail.com>
2025-02-05 08:48:46 -0800

    fix for playwright

--- playwright/esbuild.config.js
+++ playwright/esbuild.config.js
@@ -9,9 +9,8 @@ const AzureCommunicationChatPolyfills = () => {
             onResolve({filter: /@azure\/communication-chat/}, args => {
                 // Polyfill Chat Package to support real-time notifications on Browser
                 if (args.path === '@azure/communication-chat') {
-                    const defaultPath = require.resolve('@azure/communication-chat');
-                    const basePath = path.dirname(path.dirname(defaultPath))
-                    const esmPath = path.join(basePath, 'dist-esm', 'src','index.js');
+                    const defaultPath = path.resolve(__dirname, '../node_modules/@microsoft/botframework-webchat-adapter-azure-communication-chat/node_modules/@azure/communication-chat');
+                    const esmPath = path.join(defaultPath, 'dist-esm', 'src', 'index.js');
                     return {
                         path: esmPath.toString()
                     }
