112bad8a8d10ab79b4f1794c0ad25f8107d784bb
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-02-28 14:29:22 -0800

    Add command method to stop polling

---
+++ src/commands/stopPolling.ts
@@ -0,0 +1,32 @@
+import LiveChatVersion from "../core/LiveChatVersion";
+import { ACSConversation } from "../core/messaging/ACSClient";
+import ScenarioMarker from "../telemetry/ScenarioMarker";
+import TelemetryEvent from "../telemetry/TelemetryEvent";
+import exceptionThrowers from "../utils/exceptionThrowers";
+
+const stopPolling = async (isInitialized: boolean, scenarioMarker: ScenarioMarker, liveChatVersion: number, requestId: string, chatId: string, conversation: ACSConversation | null): Promise<void> => {
+    scenarioMarker.startScenario(TelemetryEvent.StopPolling);
+    
+    if (!isInitialized) {
+        exceptionThrowers.throwUninitializedChatSDK(scenarioMarker, TelemetryEvent.StopPolling);
+    }
+
+    if (liveChatVersion === LiveChatVersion.V2) {
+        try {
+            await conversation?.stopPolling();
+            scenarioMarker.completeScenario(TelemetryEvent.StopPolling);
+        } catch (error) {
+            const exceptionDetails = {
+                errorObject: `${error}`
+            };
+
+            scenarioMarker.failScenario(TelemetryEvent.StopPolling, {
+                RequestId: requestId,
+                ChatId: chatId as string,
+                ExceptionDetails: JSON.stringify(exceptionDetails)
+            });
+        }
+    }
+}
+
+export default stopPolling;
