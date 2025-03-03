97a1ae3d6281fd796da8a4ac12965648f334c5f5
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-03-03 15:32:10 -0800

    Add property to disable polling

--- src/core/messaging/ACSRegisterOnNewMessageOptionalParams.ts
+++ src/core/messaging/ACSRegisterOnNewMessageOptionalParams.ts
@@ -1,3 +1,4 @@
 export default interface ACSRegisterOnNewMessageOptionalParams {
     pollingInterval?: number; // Polling interval in milliseconds
+    disablePolling: boolean; // Disable polling
 }
--- src/core/messaging/OnNewMessageOptionalParams.ts
+++ src/core/messaging/OnNewMessageOptionalParams.ts
@@ -1,4 +1,5 @@
 export default interface OnNewMessageOptionalParams {
     rehydrate?: boolean;
     pollingInterval?: number;
+    disablePolling?: boolean;
 }
