ae6567c2f0985bd0a7995ce009750b39506f5d3b
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-03-26 16:55:17 -0700

    Add test

--- __tests__/utils/createOmnichannelMessage.spec.ts
+++ __tests__/utils/createOmnichannelMessage.spec.ts
@@ -248,4 +248,56 @@ describe('createOmnichannelMessage', () => {
             url: ''
         });
     });
+
+    it('createOmnichannelMessage with LiveChatV2 messaging contracts with \'OriginalMessageId\' should return OmnichannelMessage contracts', () => {
+        const amsReferences = ['id'];
+        const amsMetadata = [{fileName: 'fileName.ext', size: 0, contentType: 'type'}]
+        const sampleMessage = {
+            id: 'id',
+            content: 'content',
+            metadata: {
+                tags: 'tags',
+                amsMetadata: JSON.stringify(amsMetadata),
+                amsReferences: JSON.stringify(amsReferences),
+                OriginalMessageId: 'originalMessageId'
+            },
+            sender: {
+                communicationUserId: 'id',
+                kind: "communicationUser"
+            },
+            senderDisplayName: 'senderDisplayName',
+            createdOn: 'createdOn'
+        };
+
+        const omnichannelMessage = createOmnichannelMessage(sampleMessage as any, {
+            liveChatVersion: LiveChatVersion.V2
+        });
+
+        expect(omnichannelMessage.id).toBeDefined();
+        expect(omnichannelMessage.messageid).toBe(undefined);
+        expect(omnichannelMessage.clientmessageid).toBe(undefined);
+        expect(omnichannelMessage.deliveryMode).toBe(undefined);
+        expect(omnichannelMessage.content).toBe(sampleMessage.content);
+        expect(omnichannelMessage.properties).toBeDefined();
+        expect(omnichannelMessage.tags).toEqual(sampleMessage.metadata.tags.split(','));
+        expect(omnichannelMessage.timestamp).toBe(sampleMessage.createdOn);
+        expect(omnichannelMessage.messageType).toBe(MessageType.UserMessage);
+        expect(omnichannelMessage.sender).toEqual({
+            id: sampleMessage.sender.communicationUserId,
+            displayName: sampleMessage.senderDisplayName,
+            type: PersonType.Bot
+        });
+        expect(omnichannelMessage.fileMetadata).toEqual({
+            fileSharingProtocolType: 0,
+            id: amsReferences[0],
+            name: amsMetadata[0].fileName,
+            size: 0,
+            type: amsMetadata[0].contentType,
+            url: ''
+        });
+
+        if (omnichannelMessage.properties) {
+            expect(omnichannelMessage.properties.originalMessageId).toEqual(sampleMessage.metadata.OriginalMessageId);
+        }
+    });
 });
