37507cc1a311ef907ce50ec7cb43aa1b7b7c06e2
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-03-26 15:10:34 -0700

    Fix empty space

--- playwright/utils/fetchTestConfig.ts
+++ playwright/utils/fetchTestConfig.ts
@@ -8,7 +8,7 @@ const fetchTestConfig = () => {
     }
 
     const testConfigFilePath = path.join(path.dirname(__dirname), 'test.config.yml');
-    
+
     let testConfig = null;
     try {
         const fileData = fs.readFileSync(testConfigFilePath, 'utf8');
