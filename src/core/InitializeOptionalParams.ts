04fcce100a92d6d7caecc20cfea52b49f463bdd6
Edgar Lopez <elopezanaya@gmail.com>
2025-04-07 14:24:58 -0700

    changes

--- src/core/InitializeOptionalParams.ts
+++ src/core/InitializeOptionalParams.ts
@@ -1,22 +1,7 @@
 import GetLiveChatConfigOptionalParams from "./GetLiveChatConfigOptionalParams"
 
-/**
- * Interface representing optional parameters for initialization.
- *
- * @property getLiveChatConfigOptionalParams - Optional parameters for retrieving the live chat configuration.
- * @property useParallelLoad - deprecated. this is not longer evaluated but was not removed for backward compatibility.
- * @property useSequentialLoad - Indicates whether to load resources sequentially. If set to `true`, resources will be loaded one after another in sequence.
- */
 interface InitializeOptionalParams {
-    /**
-     * Optional parameters for retrieving the live chat configuration.
-     */
     getLiveChatConfigOptionalParams?: GetLiveChatConfigOptionalParams;
-
-    /**
-     * Indicates whether to load resources in parallel.
-     * Not longer evaluated but was not removed for backward compatibility.
-     */
     useParallelLoad?: boolean;
 }
 
