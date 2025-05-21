bcf296d850cf6f3a65b33aef4a35ad3b2dc9e6c6
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-05-21 12:24:23 -0700

    Add tests

---
+++ __tests__/utils/AMSClientUtils.web.spec.ts
@@ -0,0 +1,43 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { whitelistedUrls, isUrlWhitelisted, shouldUseFramedMode } from "../../src/utils/AMSClientUtils";
+
+describe("AMSClientUtils", () => {
+    const { location } = window;
+
+    afterAll((): void => {
+        (window as any).location = location;
+    });
+
+    it("isUrlWhitelisted() should return 'true' if URL is whitelisted", () => {
+        for (const url of whitelistedUrls) {
+            expect(isUrlWhitelisted(url)).toBe(true);
+        }
+    });
+
+    it("isUrlWhitelisted() should return 'false' if URL is not whitelisted", () => {
+        const url = "https://contoso.com";
+        expect(isUrlWhitelisted(url)).toBe(false);
+    });
+
+    it("shouldUseFramedMode() should return 'false' if URL is whitelisted", () => {
+        for (const url of whitelistedUrls) {
+            delete (window as any).location;
+            (window as any).location = {
+                origin: url,
+            };
+            expect(shouldUseFramedMode()).toBe(false);
+        }
+    });
+
+    it("shouldUseFramedMode() should return 'true' if URL is not whitelisted", () => {
+        const url = "https://contoso.com";
+        delete (window as any).location;
+        (window as any).location = {
+            origin: url,
+        };
+        expect(shouldUseFramedMode()).toBe(true);
+    });
+});
