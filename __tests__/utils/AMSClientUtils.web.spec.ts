50d59ebca5c53bbf01e963878614369a26f38f4b
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2025-06-18 14:09:23 -0700

    Add tests

--- __tests__/utils/AMSClientUtils.web.spec.ts
+++ __tests__/utils/AMSClientUtils.web.spec.ts
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { whitelistedUrls, isUrlWhitelisted, shouldUseFramedMode, regionBasedSupportedUrls, isRegionBasedUrlSupported, retrieveRegionBasedUrl } from "../../src/utils/AMSClientUtils";
+import { whitelistedUrls, isUrlWhitelisted, shouldUseFramedMode, regionBasedSupportedUrls, regionBasedUrlSupportedVersions, isRegionBasedUrlSupported, retrieveRegionBasedUrl } from "../../src/utils/AMSClientUtils";
 
 describe("AMSClientUtils", () => {
     const { location } = window;
@@ -104,6 +104,30 @@ describe("AMSClientUtils", () => {
         ];
 
         for (const url of unsupportedUrl) {
+            const regionBasedUrl = retrieveRegionBasedUrl(url);
+            expect(regionBasedUrl).toBe("");
+        }
+    });
+
+    it("retrieveRegionBasedUrl() should NOT return '' if version is supported", () => {
+        const url = regionBasedSupportedUrls[0];
+        for (const version of regionBasedUrlSupportedVersions) {
+            jest.replaceProperty(require('@microsoft/omnichannel-amsclient/package.json'), 'version', version);
+            const regionBasedUrl = retrieveRegionBasedUrl(url);
+            expect(regionBasedUrl).not.toBe("");
+        }
+    });
+
+    it("retrieveRegionBasedUrl() should return '' if version is not supported", () => {
+        const url = regionBasedSupportedUrls[0];
+        const unsupportedVersions = [
+            "0.1.5",
+            "0.1.6",
+            "0.1.7",
+        ];
+
+        for (const version of unsupportedVersions) {
+            jest.replaceProperty(require('@microsoft/omnichannel-amsclient/package.json'), 'version', version);
             const regionBasedUrl = retrieveRegionBasedUrl(url);
             expect(regionBasedUrl).toBe("");
         }
